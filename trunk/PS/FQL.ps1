b7f3c0c6eaf1ac00907b1aeb2eec7f3f8910d75f
heldersepu@gmail.com <heldersepu@gmail.com@b6f7eef8-fe21-8dd7-1aa9-6d8af8908fb6>
2014-09-16 19:42:58 +0000

    test
    Former-commit-id: 820ba3f60dd6b46612fa5d4e4142cbd52535a7e8

--- trunk/PS/FQL.ps1
+++ trunk/PS/FQL.ps1
@@ -2,4 +2,21 @@ $site = Get-SPSite http://nyc-dc1
 
 
 $kg = New-Object Microsoft.Office.Server.Search.Query.KeywordQuery $site
+$kg.ResultTypes = [Microsoft.Office.Server.Search.Query.ResultType]::RelevantResults
+
+$kg.RowLimit = 1
+$kg.EnableFQL = 1
+$kg.QueryText = "cat"
+
+$resTC = $kg.Execute()
+
+Write-Host Total Hits: $resTC[1].TotalRows
+
+$rTab = $resTC.Item([Microsoft.Office.Server.Search.Query.ResultType]::RelevantResults)
+$relDataTable = $rTab.Table
+$relDataTable | Format-Table -AutoSize -Property title, url$site = Get-SPSite http://nyc-dc1
+
+
+$kg = New-Object Microsoft.Office.Server.Search.Query.KeywordQuery $site
 $kg.ResultTypes = []::RelevantResults
+
