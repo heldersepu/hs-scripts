df706fcdd35819b7dd2b9bb4b1e72db046f21646
heldersepu <heldersepu@gmail.com>
2014-09-16 20:27:11 +0000

    add a check for PSSnapin
    Former-commit-id: f452949b5e81b06511907cd7be632567c1b3f2ca

--- PS/FQL.ps1
+++ PS/FQL.ps1
@@ -1,5 +1,10 @@
-Add-PSSnapin "Microsoft.SharePoint.PowerShell"
- 
+$ver = $host | select version
+if ($ver.Version.Major -gt 1) {$host.Runspace.ThreadOptions = "ReuseThread"} 
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
 $site = Get-SPSite http://nyc-dc1
 
 
