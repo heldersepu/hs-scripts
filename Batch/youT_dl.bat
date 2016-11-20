a00e92ff7142fba26104abc28db584e69e59c9a3
HelderSepu <heldersepu@gmail.com>
2016-11-20 09:20:59 -0500

    Sample batch using youtube-dl

---
+++ Batch/youT_dl.bat
@@ -0,0 +1,9 @@
+I:
+CD I:\Videos\YT
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=uUOp8sK3LB8
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=Su01EalFJqA
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=Tcb_P5yQ5qU
+
+@ECHO.
+@ECHO    DONE !!
+@PAUSE
