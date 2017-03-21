6e85e4d72fef11bc4dcca03b985af3c8a9a50e33
HelderSepu <HelderSepu@gmail.com>
2017-03-21 16:13:33 -0400

    Add a few more common locations
    Former-commit-id: 251999f4c36a0288bfaa3b66abd94e7008277ecf

--- Batch/delBinObj.bat
+++ Batch/delBinObj.bat
@@ -4,12 +4,27 @@
 @CD "D:\SourceCode\Global"
 
 @FOR /F "tokens=1* delims=|" %%a IN ('DIR /AD /S /B obj') DO @(
-	ECHO /S /Q "%%a"
+	ECHO "%%a"
 	RMDIR /S /Q "%%a"
 )
 
 @FOR /F "tokens=1* delims=|" %%a IN ('DIR /AD /S /B bin') DO @(
-	ECHO /S /Q "%%a"
+	ECHO "%%a"
+	RMDIR /S /Q "%%a"
+)
+
+@FOR /F "tokens=1* delims=|" %%a IN ('DIR /AD /S /B Packages') DO @(
+	ECHO "%%a"
+	RMDIR /S /Q "%%a"
+)
+
+@FOR /F "tokens=1* delims=|" %%a IN ('DIR /AD /S /B TestResults') DO @(
+	ECHO "%%a"
+	RMDIR /S /Q "%%a"
+)
+
+@FOR /F "tokens=1* delims=|" %%a IN ('DIR /AD /S /B FakesAssemblies') DO @(
+	ECHO "%%a"
 	RMDIR /S /Q "%%a"
 )
 
