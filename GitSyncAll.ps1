91582fb639789870e6fabc8fbcba7f931623f313
HelderSepu <HelderSepu@gmail.com>
2017-07-31 08:24:10 -0400

    change git sync to git pull

--- GitSyncAll.ps1
+++ GitSyncAll.ps1
@@ -1,2 +1,2 @@
 cd..
-Get-ChildItem -Directory | foreach-object -process { echo .; echo $_; cd $_; git sync; cd ..}
+Get-ChildItem -Directory | foreach-object -process { echo .; echo $_; cd $_; git pull; cd ..}
