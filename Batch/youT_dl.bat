0aa1fd1fdf3e94bfde79bcb78a34be494b43aafa
HelderSepu <heldersepu@gmail.com>
2017-09-08 08:09:12 -0400

    Add some more videos
    Former-commit-id: 496c548219ae11133e6d23bd13b3c6ea3cca4c70

--- Batch/youT_dl.bat
+++ Batch/youT_dl.bat
@@ -1,8 +1,51 @@
 I:
 CD I:\Videos\YT
+
 C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=uUOp8sK3LB8
 C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=Su01EalFJqA
 C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=Tcb_P5yQ5qU
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=03jHBpy1I7I
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=X4REf3H6WnQ
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=-YUpiQquFFE
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=fSt8WNERRG0
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=UG_8b6WJqvI
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=m8BJNK1WICE
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=KZyshh9dseQ
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=z_aVNv_gNdM
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=woNyVWjndAQ
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=2jDOj0Xhcc8
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=2jDOj0Xhcc8
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=EQvIlYPmfNE
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=-JhnDL8nUME
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=YhCY_6rf-HQ
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=tMyNTuw9Njs
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=tY3HlvsEkW8
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=Fs7aw3mA22I
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=3oyS_RMzGkM
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=QneHPr3vuds
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=DU3B2P6pc2U
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=qdo8KpJyHSg
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=51XLjg7lasY
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=wd5sO3gRGeo
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=HXFnrsspw78
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=vdOkdIOl8oM
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=rs5k2sz87Po
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=PaOY8lMFFGo
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=lReHGNMwHoM
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=UMXB2cfwdtU
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=PxBUMt64V64
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=Xw3DD_L-xI0
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=YmrIstgjo7c
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=acedEJ7Ya-s
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=HcH6COd5kcU
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=66bQOWYmtC4
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=A6mw7km4caM
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=mBmX-3O63_0
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=QvA4kRwoYzg
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=I4LsGtQZaT8
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=UBO6641Mwik
+C:\Windows\youtube-dl.exe --format mp4 https://www.youtube.com/watch?v=YvAwmF8Y7Ok
+::C:\Windows\youtube-dl.exe --format mp4 
 
 @ECHO.
 @ECHO    DONE !!
