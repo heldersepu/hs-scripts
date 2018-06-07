e6f737783dd066718cf47ed13049b4159b043022
Helder Sepulveda <heldersepu@gmail.com>
2018-06-07 08:21:19 -0400

    Move the terraform output to own file

--- TerraForm/example.tf
+++ TerraForm/example.tf
@@ -34,27 +34,3 @@ resource "aws_subnet" "app1" {
   availability_zone       = "${data.aws_availability_zones.available.names[0]}"
   map_public_ip_on_launch = false
 }
-
-output "aws_availability_zones_names" {
-  value = "${data.aws_availability_zones.available.names}"
-}
-
-output "aws_s3_bucket_name" {
-  value = "${aws_s3_bucket.sample_bucket12629.id}"
-}
-
-output "aws_subnet_cidr_block" {
-  value = "${aws_subnet.app1.cidr_block}"
-}
-
-output "aws_subnet_name" {
-  value = "${aws_subnet.app1.id}"
-}
-
-output "aws_vpc_cidr_block" {
-  value = "${aws_vpc.myvpc.cidr_block}"
-}
-
-output "aws_vpc_name" {
-  value = "${aws_vpc.myvpc.id}"
-}
