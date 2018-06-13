f324b900ddab870eeaa122cd179bdf7fce414ac2
Helder Sepulveda <heldersepu@gmail.com>
2018-06-13 17:56:27 -0400

    Testing region = "us-east-1"

--- TerraForm/output.tf
+++ TerraForm/output.tf
@@ -6,25 +6,25 @@ output "aws_availability_zones_names_len" {
   value = "${length(data.aws_availability_zones.available.names)}"
 }
 
-output "aws_s3_bucket_name" {
-  value = "${aws_s3_bucket.sample_bucket12629.*.id}"
-}
-
-output "aws_subnet_cidr_block" {
-  value = "${aws_subnet.app1.*.cidr_block}"
-}
-
-output "aws_subnet_name" {
-  value = "${aws_subnet.app1.*.id}"
-}
-
-output "aws_vpc_cidr_block" {
-  value = "${aws_vpc.myvpc.*.cidr_block}"
-}
-
-output "aws_vpc_name" {
-  value = "${aws_vpc.myvpc.*.id}"
-}
+# output "aws_s3_bucket_name" {
+#   value = "${aws_s3_bucket.sample_bucket12629.*.id}"
+# }
+#
+# output "aws_subnet_cidr_block" {
+#   value = "${aws_subnet.app1.*.cidr_block}"
+# }
+#
+# output "aws_subnet_name" {
+#   value = "${aws_subnet.app1.*.id}"
+# }
+#
+# output "aws_vpc_cidr_block" {
+#   value = "${aws_vpc.myvpc.*.cidr_block}"
+# }
+#
+# output "aws_vpc_name" {
+#   value = "${aws_vpc.myvpc.*.id}"
+# }
 
 output "testing_lookup" {
   value = "${lookup(var.ip_ranges, "de", "10.10.10.0/24")}"
