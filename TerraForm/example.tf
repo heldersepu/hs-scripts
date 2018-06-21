846f333a6281058f3416963fa3b32563f12d0658
Helder Sepulveda <heldersepu@gmail.com>
2018-06-21 17:41:25 -0400

    Move VPC stuff to own file
    Former-commit-id: 5a8700c258c1ab42eb441de18578215ebeb7f88d

--- TerraForm/example.tf
+++ TerraForm/example.tf
@@ -21,28 +21,3 @@ resource "aws_s3_bucket" "sample_bucket12629" {
     Environment = "Dev"
   }
 }
-
-resource "aws_vpc" "myvpc" {
-  count                = "${var.enabled}"
-  cidr_block           = "10.0.128.0/24"
-  instance_tenancy     = "default"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  enable_classiclink   = false
-
-  tags {
-    Name = "aws_vpc"
-  }
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-resource "aws_subnet" "app1" {
-  count                   = "${var.enabled}"
-  vpc_id                  = "${aws_vpc.myvpc.id}"
-  cidr_block              = "${cidrsubnet("10.0.128.0/24",2,0)}"
-  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
-  map_public_ip_on_launch = false
-}
