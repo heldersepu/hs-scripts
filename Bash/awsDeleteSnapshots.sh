3b3145eaac50990e6cc0f12ca39dd5cec351d425
Helder Sepulveda <heldersepu@gmail.com>
2018-06-21 14:32:17 -0400

    Speed up the deletion of snapshots
     send one call to background one to foreground

--- Bash/awsDeleteSnapshots.sh
+++ Bash/awsDeleteSnapshots.sh
@@ -2,21 +2,26 @@ RED='\033[0;31m'
 GRN='\033[0;32m'
 NC='\033[0m' # No Color
 
-declare -a sizes=("600" "300" "200" "60" "50" "20")
+declare -a sizes=("1024" "600" "500" "300" "200" "60" "50" "20" "10" "1")
 
 if [ -z ${SWAPEX_ENVIRONMENT+x} ]  ; then
     echo "${RED}You need to set your SWAPEX_ENVIRONMENT variables${NC}" ;
 else
     for s in "${sizes[@]}"
     do
-        arr=($(aws ec2 describe-snapshots --filters Name=volume-size,Values=${s} | jq -r '.Snapshots[].SnapshotId'))
-        for i in "${arr[@]}"
+        arr=($(aws ec2 describe-snapshots --owner-ids 998877665544 --filters Name=volume-size,Values=${s} | jq -r '.Snapshots[].SnapshotId'))
+        
+        min=0
+        max=$(( ${#arr[@]} -1 ))
+
+        while [[ min -lt max ]]
         do
-            if [ ${#i} -gt 15 ] ; then
-                echo "${RED}${s} ${GRN} ${i} ${NC}"
-                aws ec2 delete-snapshot --region us-east-1 --snapshot-id ${i}
-                echo ""
-            fi
+            echo "${RED}${s} ${GRN} ${arr[$min]} ${NC}"
+            echo "${RED}${s} ${GRN} ${arr[$max]} ${NC}"
+            aws ec2 delete-snapshot --region us-east-1 --snapshot-id ${arr[$min]} &
+            aws ec2 delete-snapshot --region us-east-1 --snapshot-id ${arr[$max]}
+            echo ""
+            (( min++, max-- ))
         done
         echo " -- -- -- -- -- -- -- -- -- -- -- -- -- "
     done
