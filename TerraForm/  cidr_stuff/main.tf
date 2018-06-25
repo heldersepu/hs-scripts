974b8ea7f8c9e719d3bd3ac5addaf9be4d466c5f
Helder Sepulveda <heldersepu@gmail.com>
2018-06-25 17:26:26 -0400

    Testing cidrsubnet
    Former-commit-id: c8bab26d3a4998c84a0eee3a306141d5ee281d1b

---
+++ TerraForm/  cidr_stuff/main.tf
@@ -0,0 +1,27 @@
+locals {
+  "cidr_range" = "10.35.112.0/21"
+}
+
+output "cidr_1__4-0" {
+  value = "${cidrsubnet(local.cidr_range, 4, 0)}"
+}
+
+output "cidr_2__4-1" {
+  value = "${cidrsubnet(local.cidr_range, 4, 1)}"
+}
+
+output "cidr_3__3-1" {
+  value = "${cidrsubnet(local.cidr_range, 3, 1)}"
+}
+
+output "cidr_4__2-1" {
+  value = "${cidrsubnet(local.cidr_range, 2, 1)}"
+}
+
+output "cidr_5__2-2" {
+  value = "${cidrsubnet(local.cidr_range, 2, 2)}"
+}
+
+output "cidr_6__2-3" {
+  value = "${cidrsubnet(local.cidr_range, 2, 3)}"
+}
