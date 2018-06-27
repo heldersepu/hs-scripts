f8219c7172bcfd34b73844ee4d2e14e56f78d90d
Helder Sepulveda <heldersepu@gmail.com>
2018-06-27 17:01:23 -0400

    Testing aws_kinesis_stream
    Former-commit-id: 08a1ad6617402058db40bb8a697141f7f3ad660a

---
+++ TerraForm/cloudwatch/main.tf
@@ -0,0 +1,45 @@
+data "aws_iam_policy_document" "drole" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "logs.us-east-1.amazonaws.com",
+        "logs.us-east-2.amazonaws.com",
+      ]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_role" {
+  name               = "iam_role"
+  assume_role_policy = "${data.aws_iam_policy_document.drole.json}"
+}
+
+resource "aws_kinesis_stream" "kinesis_stream" {
+  name             = "kinesis_stream"
+  shard_count      = 1
+  retention_period = 24
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = "log_group"
+  retention_in_days = "14"
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "log_subs_filter" {
+  name            = "log_subs_filter"
+  role_arn        = "${aws_iam_role.iam_role.arn}"
+  log_group_name  = "${aws_cloudwatch_log_group.log_group.name}"
+  filter_pattern  = "[]"
+  destination_arn = "${aws_kinesis_stream.kinesis_stream.arn}"
+}
