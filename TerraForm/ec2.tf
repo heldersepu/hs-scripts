37f673b81112fd5b3d17411ae3ce01c289ff989e
Helder Sepulveda <heldersepu@gmail.com>
2018-06-28 19:59:58 -0400

    Testing ebs_block_device

--- TerraForm/ec2.tf
+++ TerraForm/ec2.tf
@@ -14,13 +14,25 @@ resource "aws_instance" "win2016" {
 resource "aws_instance" "suse12" {
   count                  = "${var.ec2_enabled}"
   ami                    = "${data.aws_ami.suse12.id}"
-  instance_type          = "m5.xlarge"
+  instance_type          = "m5.large"
   key_name               = "${aws_key_pair.sshkey.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+
+  ebs_block_device {
+    device_name           = "/dev/sdg"
+    volume_type           = "gp2"
+    volume_size           = "25"
+    delete_on_termination = true
+  }
 
   tags {
     Terraformed = "true"
     Name        = "suse12"
+  }
+
+  volume_tags {
+    Terraformed = "true"
+    Name        = "suse12 volume"
   }
 }
 
