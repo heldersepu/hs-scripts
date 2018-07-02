3d6b30c6bbec0601d322a6ba14a637bee74178f5
Helder Sepulveda <heldersepu@gmail.com>
2018-07-02 11:15:10 -0400

    Clean up VPN changes

--- TerraForm/vpn.tf
+++ TerraForm/vpn.tf
@@ -1,39 +1,41 @@
-resource "aws_vpn_gateway" "dxconn" {
-  tags = {
-    Name        = "Direct Connect"
-    Terraformed = "true"
-  }
-}
-
 resource "aws_customer_gateway" "gate" {
   bgp_asn    = 65533
   ip_address = "172.83.124.10"
   type       = "ipsec.1"
 
   tags {
-    Name        = "the gate"
-    Terraformed = "true"
+    Name = "the gate"
+  }
+}
+
+resource "aws_vpn_gateway" "west" {
+  tags = {
+    Name = "west_vpn_gateway"
   }
 }
 
 resource "aws_vpn_connection" "west" {
-  vpn_gateway_id      = "${aws_vpn_gateway.dxconn.id}"
+  vpn_gateway_id      = "${aws_vpn_gateway.west.id}"
   customer_gateway_id = "${aws_customer_gateway.gate.id}"
   type                = "ipsec.1"
 
   tags {
-    Name        = "Backup VPN West"
-    Terraformed = "true"
+    Name = "The VPN West"
+  }
+}
+
+resource "aws_vpn_gateway" "east" {
+  tags = {
+    Name = "east_vpn_gateway"
   }
 }
 
 resource "aws_vpn_connection" "east" {
-  vpn_gateway_id      = "${aws_vpn_gateway.dxconn.id}"
+  vpn_gateway_id      = "${aws_vpn_gateway.east.id}"
   customer_gateway_id = "${aws_customer_gateway.gate.id}"
   type                = "ipsec.1"
 
   tags {
-    Name        = "Backup VPN East"
-    Terraformed = "true"
+    Name = "The VPN East"
   }
 }
