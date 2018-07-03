e2385809cb0328989cf206a79968c6be9d0b2f2b
Helder Sepulveda <heldersepu@gmail.com>
2018-07-03 17:00:24 -0400

    Add win_ec2_enabled

--- TerraForm/ec2.tf
+++ TerraForm/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "win2016" {
-  count                  = "${var.ec2_enabled}"
+  count                  = "${var.win_ec2_enabled}"
   ami                    = "${data.aws_ami.win2016.id}"
   instance_type          = "m5.large"
   key_name               = "${aws_key_pair.sshkey.key_name}"
@@ -19,7 +19,7 @@ resource "aws_instance" "win2016" {
 resource "aws_instance" "suse12" {
   count                  = "${var.ec2_enabled}"
   ami                    = "${data.aws_ami.suse12.id}"
-  instance_type          = "m5.large"
+  instance_type          = "m5d.large"
   key_name               = "${aws_key_pair.sshkey.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   availability_zone      = "${data.aws_availability_zones.available.names[0]}"
@@ -33,7 +33,7 @@ resource "aws_instance" "suse12" {
 
   ephemeral_block_device {
     virtual_name = "ephemeral0"
-    device_name  = "xvdb"
+    device_name  = "/dev/sde"
   }
 
   tags {
