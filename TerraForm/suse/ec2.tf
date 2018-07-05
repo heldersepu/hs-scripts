45ecda2ce2dd518dc8dd3c0041caa3218838f92d
Helder Sepulveda <heldersepu@gmail.com>
2018-07-04 22:25:29 -0400

    reproducing bug with tags
    Former-commit-id: 6c1210759b7c96b635b12da26e760c4c7a49c1cd

---
+++ TerraForm/suse/ec2.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "suse12" {
+  filter {
+    name   = "image-id"
+    values = ["ami-1c761163"]
+  }
+}
+
+resource "aws_instance" "suse12" {
+  ami               = "${data.aws_ami.suse12.id}"
+  instance_type     = "m5d.large"
+  availability_zone = "us-east-1a"
+
+  volume_tags {
+    Name = "suse12 volume tags"
+  }
+}
+
+resource "aws_ebs_volume" "data-ndj" {
+  type              = "gp2"
+  size              = 110
+  availability_zone = "us-east-1a"
+
+  tags {
+    Name = "suse12-data-ndj"
+  }
+}
+
+resource "aws_volume_attachment" "data-ndj" {
+  device_name = "/dev/sdj"
+  instance_id = "${aws_instance.suse12.*.id[0]}"
+  volume_id   = "${aws_ebs_volume.data-ndj.*.id[0]}"
+}
