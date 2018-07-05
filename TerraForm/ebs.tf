ea262043aa0b4a7731c2d57c78c2d57a258bee7e
Helder Sepulveda <heldersepu@gmail.com>
2018-07-04 21:30:31 -0400

    Add own variable for aws_ebs_volume
    Former-commit-id: d44703720136993756970ed56f991fe3ccc66d58

--- TerraForm/ebs.tf
+++ TerraForm/ebs.tf
@@ -1,7 +1,7 @@
 resource "aws_ebs_volume" "data-ndj" {
-  count             = "${var.ec2_enabled}"
+  count             = "${var.ec2_add_volume}"
   type              = "gp2"
-  size              = 200
+  size              = 110
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
@@ -15,7 +15,7 @@ resource "aws_ebs_volume" "data-ndj" {
 }
 
 resource "aws_volume_attachment" "data-ndj" {
-  count       = "${var.ec2_enabled}"
+  count       = "${var.ec2_add_volume}"
   device_name = "/dev/sdj"
   instance_id = "${aws_instance.suse12.*.id[0]}"
   volume_id   = "${aws_ebs_volume.data-ndj.*.id[0]}"
