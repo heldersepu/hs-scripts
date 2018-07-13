637b96e1ff7599f2a90f0d94cefe8da84e7af00a
Helder Sepulveda <heldersepu@gmail.com>
2018-07-13 16:11:37 -0400

    Testing ELB
    Former-commit-id: c41f6be8839b2f05d6c871b809c7074a0ba16702

---
+++ TerraForm/elb.tf
@@ -0,0 +1,32 @@
+resource "aws_elb" "elb" {
+  name    = "elb"
+  subnets = ["${aws_subnet.app1.id}"]
+
+  #TODO: disable this!
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  #TODO: enable this!
+  /*
+  listener {
+    instance_port      = 8000
+    instance_protocol  = "https"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "${var.acm_cert_arn}"
+  }
+  */
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+  tags {
+    Terraformed = "true"
+    Name        = "elb"
+  }
+}
