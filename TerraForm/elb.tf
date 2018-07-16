0d1728b10e70ccb7b975b1b3643e2663b7929e63
Helder Sepulveda <heldersepu@gmail.com>
2018-07-16 15:30:44 -0400

    Change aws_load_balancer_policy

--- TerraForm/elb.tf
+++ TerraForm/elb.tf
@@ -26,3 +26,23 @@ resource "aws_subnet" "test_app1" {
   vpc_id     = "${aws_vpc.myvpc.id}"
   cidr_block = "10.0.1.0/24"
 }
+
+resource "aws_load_balancer_policy" "policy_tls_1_1" {
+  load_balancer_name = "${aws_elb.elb.name}"
+  policy_name        = "policy-tls-1-1"
+  policy_type_name   = "SSLNegotiationPolicyType"
+
+  policy_attribute {
+    name  = "Reference-Security-Policy"
+    value = "ELBSecurityPolicy-TLS-1-1-2017-01"
+  }
+}
+
+resource "aws_load_balancer_listener_policy" "vault_server_listener_policies" {
+  load_balancer_name = "${aws_elb.elb.name}"
+  load_balancer_port = 443
+
+  policy_names = [
+    "${aws_load_balancer_policy.policy_tls_1_1.policy_name}",
+  ]
+}
