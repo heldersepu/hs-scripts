f7b7253811f0e14e92b5316a55411da9dde7f681
Helder Sepulveda <heldersepu@gmail.com>
2018-07-16 13:54:44 -0400

    Testing resource "aws_acm_certificate"

---
+++ TerraForm/acm.tf
@@ -0,0 +1,37 @@
+resource "aws_acm_certificate" "default" {
+  count       = 0
+  domain_name = "test.azurewebsites.net"
+
+  # Which method to use for validation. DNS or EMAIL are valid, 
+  # NONE can be used for certificates that were imported into ACM and then into Terraform.
+  validation_method = "NONE"
+
+  subject_alternative_names = ["*.test.azurewebsites.net"]
+
+  tags = {
+    Terraformed = "true"
+  }
+}
+
+resource "aws_route53_zone" "zone" {
+  name          = "test.azurewebsites.net"
+  force_destroy = true
+}
+
+resource "aws_route53_record" "cert_validation" {
+  count   = 0
+  name    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")}"
+  type    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")}"
+  zone_id = "${aws_route53_zone.zone.id}"
+  records = ["${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_value")}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  count           = 0
+  certificate_arn = "${aws_acm_certificate.default.arn}"
+
+  timeouts {
+    create = "20m"
+  }
+}
