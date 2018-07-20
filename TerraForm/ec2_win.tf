0996c3223e0827d7aa40705cf5a3f7be5b1c8e21
Helder Sepulveda <heldersepu@gmail.com>
2018-07-20 15:39:49 -0400

    Move the ec2 to own files
    Former-commit-id: 81d3e8eec6ca1ee5174dd3fe7a7589024783e0fc

---
+++ TerraForm/ec2_win.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "win2016" {
+  count                  = "${var.win_ec2_enabled}"
+  ami                    = "${data.aws_ami.win2016.id}"
+  instance_type          = "m5.large"
+  key_name               = "${aws_key_pair.sshkey.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+
+  ephemeral_block_device {
+    virtual_name = "ephemeral0"
+    device_name  = "xvde"
+    no_device    = false
+  }
+
+  tags {
+    Terraformed = "true"
+    Name        = "win2016"
+  }
+
+  volume_tags {
+    Terraformed = "true"
+    Name        = "win2016 volume root"
+  }
+}
