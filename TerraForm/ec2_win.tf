91743920a6c6ab256fa7ea0562d655fe16a872f9
Helder Sepulveda <heldersepu@gmail.com>
2018-07-23 14:53:38 -0400

    Testing remote-exec  winrm
    Former-commit-id: fb2051e33dd0638b914d447fbea54990c63b9bd7

--- TerraForm/ec2_win.tf
+++ TerraForm/ec2_win.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "win2016" {
   instance_type          = "m5.large"
   key_name               = "${aws_key_pair.sshkey.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+  get_password_data      = true
 
   ephemeral_block_device {
     virtual_name = "ephemeral0"
@@ -19,5 +20,19 @@ resource "aws_instance" "win2016" {
   volume_tags {
     Terraformed = "true"
     Name        = "win2016 volume root"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["ipconfig > ip.log"]
+
+    connection {
+      type     = "winrm"
+      user     = "Administrator"
+      password = "${rsadecrypt(self.password_data, file("~/Downloads/AWS_keys/test.pem"))}"
+      host     = "${self.public_dns}"
+      timeout  = "10m"
+      https    = false
+      insecure = true
+    }
   }
 }
