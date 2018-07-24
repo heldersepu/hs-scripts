04c3043761637f7aee1cf6eccee6e9881c3966f2
Helder Sepulveda <heldersepu@gmail.com>
2018-07-24 15:28:46 -0400

    Testing user_data
    Former-commit-id: 650dc3eb7f791d344e51086955cb06cc173b1cb9

--- TerraForm/ec2_suse.tf
+++ TerraForm/ec2_suse.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "suse12" {
   key_name               = "${aws_key_pair.sshkey.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   availability_zone      = "${data.aws_availability_zones.available.names[0]}"
+  user_data              = "echo `date` > user_data.log"
 
   ebs_block_device {
     device_name           = "/dev/sdg"
@@ -30,7 +31,7 @@ resource "aws_instance" "suse12" {
   }
 
   lifecycle {
-    ignore_changes  = ["ami", "user_data", "instance_type", "ebs_optimized", "volume_tags", "ebs_block_device"]
+    ignore_changes  = ["ami", "instance_type", "ebs_optimized", "volume_tags", "ebs_block_device"]
     prevent_destroy = false
   }
 
