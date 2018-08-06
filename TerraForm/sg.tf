c2874f289ee050f735e0371a88b9e72e5546ad52
Helder Sepulveda <heldersepu@gmail.com>
2018-08-06 17:24:45 -0400

    Add egress rule

--- TerraForm/sg.tf
+++ TerraForm/sg.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name        = "allow_all"
     Terraformed = "true"
