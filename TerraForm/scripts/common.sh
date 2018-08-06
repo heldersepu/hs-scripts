221b82b1a6afa7beff2a7b604bd2cdc9a41d4385
Helder Sepulveda <heldersepu@gmail.com>
2018-08-06 14:42:10 -0400

    Start using the volume_mount
    Former-commit-id: 4a5b6ce5cac2036136fe940f9dda922b926463ff

--- TerraForm/scripts/common.sh
+++ TerraForm/scripts/common.sh
@@ -1,14 +1,15 @@
-function volume_id() {
-
-}
+#!/bin/bash
+echo "init `date`" >> /home/ec2-user/bin/userdata.log
 
 function volume_mount() {
-    if [ $(cat /etc/fstab | grep -c '$2 $1') == 0 ]
+    dsize=$1;  dmount=$2;
+    if [ $(cat /etc/fstab | grep -c "$dmount ext4") == 0 ]
     then
-        DNAME=$(lsblk -o NAME,SIZE -x NAME | grep '1G' | awk '{print $1}')
-        echo "volume $DNAME = `date`" >> /home/ec2-user/bin/userdata.log
-        mkfs -t $1 /dev/$DNAME
-        mkdir -p $2
-        echo "/dev/$DNAME $2 $1 defaults,nofail 0 2" >> /etc/fstab
+        DNAME=$(lsblk -o NAME,SIZE -x NAME | grep "$dsize" | awk '{print $1}')
+        DUUID=$(blkid | grep $DNAME | awk '{print $2}' | tr -d '"')
+        #yes | mkfs -t ext4 /dev/$DNAME
+        #mkdir -p $dmount
+        #echo "$DUUID $dmount ext4 defaults,nofail 0 2" >> /etc/fstab
     fi
 }
+
