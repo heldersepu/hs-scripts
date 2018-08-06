2a9f2fe7f6e7518bb90505110644429e78330bfe
Helder Sepulveda <heldersepu@gmail.com>
2018-08-06 10:34:02 -0400

    Bash functions
    Former-commit-id: 6d6adf9950c2d38c59517d581497a64f07c513cc

--- TerraForm/scripts/common.sh
+++ TerraForm/scripts/common.sh
@@ -1,3 +1,14 @@
-function volume_mount() {
+function volume_id() {
 
+}
+
+function volume_mount() {
+    if [ $(cat /etc/fstab | grep -c '$2 $1') == 0 ]
+    then
+        DNAME=$(lsblk -o NAME,SIZE -x NAME | grep '1G' | awk '{print $1}')
+        echo "volume $DNAME = `date`" >> /home/ec2-user/bin/userdata.log
+        mkfs -t $1 /dev/$DNAME
+        mkdir -p $2
+        echo "/dev/$DNAME $2 $1 defaults,nofail 0 2" >> /etc/fstab
+    fi
 }
