ccd27283f59bb898788e7b7095fc633e75a9a36b
Helder Sepulveda <heldersepu@gmail.com>
2018-08-06 17:58:13 -0400

    Collect DUUID after format

--- TerraForm/scripts/common.sh
+++ TerraForm/scripts/common.sh
@@ -6,10 +6,10 @@ function volume_mount() {
     if [ $(cat /etc/fstab | grep -c "$dmount ext4") == 0 ]
     then
         DNAME=$(lsblk -o NAME,SIZE -x NAME | grep "$dsize" | awk '{print $1}')
-        DUUID=$(lsblk -o UUID,SIZE -x NAME | grep "$dsize" | awk '{print $1}')
         echo "volume $DNAME = `date`" >> /home/ec2-user/bin/userdata.log
         yes | mkfs -t ext4 /dev/$DNAME
         mkdir -p $dmount
+        DUUID=$(lsblk -o UUID,SIZE -x NAME | grep "$dsize" | awk '{print $1}')
         echo "UUID=$DUUID $dmount ext4 defaults,nofail 0 2" >> /etc/fstab
     fi
 }
