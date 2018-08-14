accad70de02e532a1f11e023d635683f841083ae
Helder Sepulveda <heldersepu@gmail.com>
2018-08-14 15:06:10 -0400

    Add comment with all possible states

--- Bash/awsEc2match.sh
+++ Bash/awsEc2match.sh
@@ -13,15 +13,16 @@ for i in "${arr[@]}"
 do
     echo "${GRN} ${i} ${NC}"
     # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html#options
-    json=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${i}*" "Name=instance-state-name,Values=stopped")
+    # ( pending | running | shutting-down | terminated | stopping | stopped )
+    json=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${i}*" "Name=instance-state-name,Values=running")
     arrids=($(echo $json | jq -r '.Reservations[].Instances[].InstanceId'))
     prvips=($(echo $json | jq -r '.Reservations[].Instances[].PrivateIpAddress'))
     echo ""
     for id in "${arrids[@]}"
     do
         echo "${RED} ${id} ${NC}"
-        aws ec2 start-instances --instance-ids ${id} | jq -r '.StartingInstances[].CurrentState.Name'
-        #aws ec2 stop-instances --instance-ids ${id} | jq -r '.StoppingInstances[].CurrentState.Name'
+        #aws ec2 start-instances --instance-ids ${id} | jq -r '.StartingInstances[].CurrentState.Name'
+        aws ec2 stop-instances --instance-ids ${id} | jq -r '.StoppingInstances[].CurrentState.Name'
         #echo ""
     done
 done
