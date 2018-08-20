9a97c10ee5ff1f37b1411fb302a4e3ee2b3d1504
Helder Sepulveda <heldersepu@gmail.com>
2018-08-20 14:27:55 -0400

    Testing XFS on SLES

--- TerraForm/scripts/user_data.sh
+++ TerraForm/scripts/user_data.sh
@@ -1,20 +1,23 @@
 echo "userdata `date`" >> $logfile
 # ${data}"
 
-volumes=('/dev/nvme2n1' '/dev/nvme3n1' '/dev/nvme4n1')
+volumes=('/dev/xvdj') # '/dev/nvme3n1' '/dev/nvme4n1')
 attach_wait $${volumes[@]}
 
 lsblk -o NAME,UUID,SIZE >> $logfile
 echo ""  >> $logfile
 
-size_mount "110G" "/abc"
-size_mount "111G" "/def"
-size_mount "111G" "/ghi"
+name_mount "/dev/xvdj" "/abcd" "xfs"
+# size_mount "110G" "/abc"
+# size_mount "111G" "/def"
+# size_mount "111G" "/ghi"
+
+mount -a
 
 # remove docker
 sudo service docker stop
 sudo ifconfig docker0 down
 sudo sed -i '/docker/d' /etc/fstab
 sudo umount /var/lib/docker
-sudo zypper rm -y docker
+sudo zypper rm -y dockerz``
 
