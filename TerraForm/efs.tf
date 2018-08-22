a27a41e387c484644e546fa45e6f243ec3df047a
Helder Sepulveda <heldersepu@gmail.com>
2018-08-22 13:09:42 -0400

    Disable aws_efs_file_system
    Former-commit-id: 5bf44ada5551b61210ab59318ec0892ef59c427c

--- TerraForm/efs.tf
+++ TerraForm/efs.tf
@@ -1,8 +1,10 @@
 resource "aws_efs_file_system" "efs" {
+  count = 0
   creation_token = "aws_efs_file_system"
 }
 
 resource "aws_efs_mount_target" "data" {
+  count = 0
   file_system_id  = "${aws_efs_file_system.efs.id}"
   subnet_id       = "${aws_subnet.app1.id}"
   #security_groups = ["${aws_security_group.allow_all.id}"]
