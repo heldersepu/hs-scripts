d8be6d4f6a75f11d80214062659c5c4846456e5a
Helder Sepulveda <heldersepu@gmail.com>
2018-08-24 16:13:24 -0400

    Testing complex maps
    Former-commit-id: 68e61d3deb503519152ecf35c88600be58841ce4

--- TerraForm/ebs.tf
+++ TerraForm/ebs.tf
@@ -1,5 +1,5 @@
 resource "aws_ebs_volume" "datav" {
-  count             = "${var.ec2_add_volume}"
+  count             = "${var.ec2_enabled ? var.ec2_add_volume: 0}"
   type              = "gp2"
   size              = "${110 + count.index}"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
@@ -16,7 +16,7 @@ resource "aws_ebs_volume" "datav" {
 }
 
 resource "aws_volume_attachment" "datav" {
-  count       = "${var.ec2_add_volume}"
+  count       = "${var.ec2_enabled ? var.ec2_add_volume: 0}"
   device_name = "${var.device_names[count.index]}"
   instance_id = "${aws_instance.suse12.*.id[0]}"
   volume_id   = "${aws_ebs_volume.datav.*.id[count.index]}"
