9e3baefc5bc203063cf34083472b4172518053f5
Helder Sepulveda <heldersepu@gmail.com>
2018-10-03 15:40:28 -0400

    Add condition to only add if needed

--- Bash/awsEc2_userdata.sh
+++ Bash/awsEc2_userdata.sh
@@ -1,8 +1,8 @@
 
 declare -a arr=(
-    "-app-sapd"
-    "-hana-sapd"
-    "-scs-sapd"
+    "-app-sap"
+    "-hana-sap"
+    "-scs-sap"
     )
 
 RED='\033[0;31m'
@@ -11,10 +11,10 @@ NC='\033[0m' # No Color
 
 for i in "${arr[@]}"
 do
-    echo "${GRN}${i} ${NC}"
+    echo "${GRN}${i}${NC}"
     # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html#options
     # ( pending | running | shutting-down | terminated | stopping | stopped )
-    json=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${i}*" "Name=instance-state-name,Values=stopped")
+    json=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${i}*" "Name=instance-state-name,Values=running")
     arrids=($(echo $json | jq -r '.Reservations[].Instances[].InstanceId'))
     echo ""
     for id in "${arrids[@]}"
@@ -22,31 +22,41 @@ do
         echo "${RED}${id}${NC}"
         data=$(aws ec2 describe-instance-attribute --instance-id ${id} --attribute userData | jq -r '.UserData.Value' | base64 --decode)
 
-        echo "Content-Type: multipart/mixed; boundary=\"//\"" > temp.txt
-        echo "MIME-Version: 1.0" >> temp.txt
-        echo "" >> temp.txt
-        echo "--//" >> temp.txt
-        echo "Content-Type: text/cloud-config; charset=\"us-ascii\"" >> temp.txt
-        echo "MIME-Version: 1.0" >> temp.txt
-        echo "Content-Transfer-Encoding: 7bit" >> temp.txt
-        echo "Content-Disposition: attachment; filename=\"cloud-config.txt\"" >> temp.txt
-        echo "" >> temp.txt
-        echo "#cloud-config" >> temp.txt
-        echo "cloud_final_modules:" >> temp.txt
-        echo "- [scripts-user, always]" >> temp.txt
-        echo "" >> temp.txt
-        echo "--//" >> temp.txt
-        echo "Content-Type: text/x-shellscript; charset=\"us-ascii\"" >> temp.txt
-        echo "MIME-Version: 1.0" >> temp.txt
-        echo "Content-Transfer-Encoding: 7bit" >> temp.txt
-        echo "Content-Disposition: attachment; filename=\"userdata.txt\"" >> temp.txt
-        echo "" >> temp.txt
-        echo "$data" >> temp.txt
-        echo "--//" >> temp.txt
-        base64 temp.txt > temp_base64.txt
+        if [[ $data = *"Content-Transfer-Encoding"* ]]; then
+            echo "${RED}  Restart detected ${NC}"
 
-        aws ec2 modify-instance-attribute --instance-id ${id} --attribute userData --value file://temp_base64.txt
-        rm temp_base64.txt
-        rm temp.txt
+            echo "" > temp_base64.txt
+            #aws ec2 modify-instance-attribute --instance-id ${id} --attribute userData --value file://temp_base64.txt
+            rm temp_base64.txt
+        elif [[ $data = *"SSH_USERS"* ]]; then
+            echo "${RED}  Single use detected ${NC}"
+
+            echo "Content-Type: multipart/mixed; boundary=\"//\"" > temp.txt
+            echo "MIME-Version: 1.0" >> temp.txt
+            echo "" >> temp.txt
+            echo "--//" >> temp.txt
+            echo "Content-Type: text/cloud-config; charset=\"us-ascii\"" >> temp.txt
+            echo "MIME-Version: 1.0" >> temp.txt
+            echo "Content-Transfer-Encoding: 7bit" >> temp.txt
+            echo "Content-Disposition: attachment; filename=\"cloud-config.txt\"" >> temp.txt
+            echo "" >> temp.txt
+            echo "#cloud-config" >> temp.txt
+            echo "cloud_final_modules:" >> temp.txt
+            echo "- [scripts-user, always]" >> temp.txt
+            echo "" >> temp.txt
+            echo "--//" >> temp.txt
+            echo "Content-Type: text/x-shellscript; charset=\"us-ascii\"" >> temp.txt
+            echo "MIME-Version: 1.0" >> temp.txt
+            echo "Content-Transfer-Encoding: 7bit" >> temp.txt
+            echo "Content-Disposition: attachment; filename=\"userdata.txt\"" >> temp.txt
+            echo "" >> temp.txt
+            echo "$data" >> temp.txt
+            echo "--//" >> temp.txt
+            base64 temp.txt > temp_base64.txt
+
+            #aws ec2 modify-instance-attribute --instance-id ${id} --attribute userData --value file://temp_base64.txt
+            rm temp_base64.txt
+            rm temp.txt
+        fi
     done
 done
