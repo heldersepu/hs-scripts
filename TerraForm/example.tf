bf1aaa922efb51577024c03f86cec15e234e1d4a
Helder Sepulveda <heldersepu@gmail.com>
2018-10-11 11:11:59 -0400

    Testing aws_glacier_vault

--- TerraForm/example.tf
+++ TerraForm/example.tf
@@ -9,21 +9,6 @@ module "bucket11" {
   ip_ranges = "${var.ip_ranges}"
 }
 
-resource "aws_s3_bucket" "sample_bucket12629" {
-  count  = "${var.enabled * var.buckets}"
-  bucket = "my-tf-test-bucket12629"
-  acl    = "private"
-
-  lifecycle {
-    prevent_destroy = false
-  }
-
-  tags {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
 resource "aws_ebs_volume" "data" {
   count             = "1"
   availability_zone = "us-east-1a"
