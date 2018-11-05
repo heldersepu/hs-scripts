1b716bd6f3bcd3e8572b07249679339008a5e31a
Helder Sepulveda <heldersepu@gmail.com>
2018-11-05 14:24:56 -0500

    testing create_before_destroy
    Former-commit-id: 80897276aea91f488a7d53c883474e2e9d796a86

--- TerraForm/ec2_ubuntu.tf
+++ TerraForm/ec2_ubuntu.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "ubuntu" {
   key_name               = "${aws_key_pair.sshkey.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   availability_zone      = "${data.aws_availability_zones.available.names[0]}"
+  user_data              = "echo test"
 
   tags {
     Terraformed = "true"
@@ -12,7 +13,8 @@ resource "aws_instance" "ubuntu" {
   }
 
   lifecycle {
-    ignore_changes  = ["ami", "user_data", "instance_type", "ebs_optimized", "volume_tags", "ebs_block_device"]
+    ignore_changes  = ["ami", "instance_type", "ebs_optimized", "volume_tags", "ebs_block_device"]
+    create_before_destroy = true
     prevent_destroy = false
   }
 
@@ -29,5 +31,9 @@ resource "aws_instance" "ubuntu" {
       private_key = "${file("~/Downloads/AWS_keys/test.pem")}"
       host        = "${self.public_dns}"
     }
+  }
+
+  provisioner "local-exec" {
+    command = "ping -c 4 ${self.private_ip}"
   }
 }
