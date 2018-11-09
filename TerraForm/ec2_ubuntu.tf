6436d39431c2e46cbd8442fe6c49ffcf72f52e87
Helder Sepulveda <heldersepu@gmail.com>
2018-11-09 11:20:55 -0500

    Refactor provisioner "local-exec"
    Move the shell script to own file and use a template_file to render it
    
    Former-commit-id: a906b86485f4367f37bb9f4c7fa5983060debfb3

--- TerraForm/ec2_ubuntu.tf
+++ TerraForm/ec2_ubuntu.tf
@@ -34,22 +34,14 @@ resource "aws_instance" "ubuntu" {
   }
 
   provisioner "local-exec" {
-    command = <<EOF
-    if [ "${var.nslookup_check}" != "" ]; then
-      tries=0
-      while true; do
-        if [[ $tries -ge 30 ]]; then
-          echo "ERROR !"
-          exit 1;
-        elif [[ $(nslookup ${var.nslookup_check} | egrep -c "(can't find|timed out)") == 0 ]]; then
-          echo "GOOD TO GO"
-          exit 0;
-        fi
-        let "tries++"
-        echo "Invalid response, will try again in 10 seconds. ($tries/30)"
-        sleep 10
-      done
-    fi
-EOF
+    command = "${data.template_file.nslookup_check.rendered}"
+  }
+}
+
+data "template_file" "nslookup_check" {
+  template = "${file("${path.module}/nslookup_check.sh")}"
+
+  vars {
+    nslookup_record = "${var.nslookup_record}"
   }
 }
