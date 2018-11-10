68e8ba8fe9ab8edde6b5982e6bfc085b6f1e5345
Helder Sepulveda <heldersepu@gmail.com>
2018-11-10 10:40:57 -0500

    Refactor loop to use for
    Former-commit-id: abb0e6cb3ddc7af0db3e8c341ce97b279bb434dd

--- TerraForm/nslookup_check.sh
+++ TerraForm/nslookup_check.sh
@@ -1,15 +1,12 @@
 if [ "${nslookup_record}" != "" ]; then
-    tries=0
-    while true; do
-    if [[ $tries -ge 30 ]]; then
-        echo "ERROR !"
-        exit 1;
-    elif [[ $(nslookup ${nslookup_record} | egrep -c "(can't find|timed out)") == 0 ]]; then
-        echo "GOOD TO GO"
-        exit 0;
-    fi
-    let "tries++"
-    echo "Invalid response, will try again in 10 seconds. ($tries/30)"
-    sleep 10
+    for tries in $(seq 1 30); do
+        if [[ $(nslookup ${nslookup_record} | egrep -c "(can't find|timed out)") == 0 ]]; then
+            echo "GOOD TO GO"
+            exit 0;
+        fi
+        echo "Invalid response, will try again in 10 seconds. ($tries/30)"
+        sleep 10
     done
+    echo "ERROR !"
+    exit 1;
 fi
