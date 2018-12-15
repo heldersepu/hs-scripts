8f039f286f31e18977f40d81be410fcdfbcfa0c0
Helder Sepulveda <heldersepu@gmail.com>
2018-12-15 11:35:23 -0500

    Testing NodeJS

---
+++ NodeJS/function.js
@@ -0,0 +1,5 @@
+const sum1 = (a, b) => a + b;
+const sum2 = (a, b) => b + a;
+
+module.exports.sum1 = sum1;
+module.exports.sum2 = sum2;
---
+++ NodeJS/main.js
@@ -0,0 +1,2 @@
+const funct = require('./function');
+console.log(funct.sum1(3,5))
