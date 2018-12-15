bb5c8f9c0f8dd271db7cb927d3cca8456678bada
Helder Sepulveda <heldersepu@gmail.com>
2018-12-15 11:35:23 -0500

    Testing NodeJS
    Former-commit-id: 8f039f286f31e18977f40d81be410fcdfbcfa0c0

---
+++ NodeJS/events.js
@@ -0,0 +1,30 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('sum',  (a, b) => {
+    console.log(a+b)
+})
+
+eventEmitter.emit('sum',5 ,3);
+
+class Car extends EventEmitter {
+    constructor(make) {
+        super();        
+        this._make = make;
+
+        this.on('honk', () =>{
+            console.log("beep beep");
+        });
+    }
+
+    get make() {
+        return this._make;
+    }
+
+    get wheels() {
+        return 4;
+    }
+}
+
+const myCar = new Car("Jeep");
+myCar.emit('honk');
