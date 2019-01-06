c222534ac2dcca57118794918e9f411b323c9f91
Helder Sepulveda <heldersepu@gmail.com>
2019-01-06 11:02:07 -0500

    Add interpreter to local exec

--- TerraForm/nslookup_check.sh
+++ TerraForm/nslookup_check.sh
@@ -2,13 +2,13 @@
 
 if [ "${nslookup_record}" != "" ]; then
     for tries in $(seq 1 30); do
-        if [[ $(nslookup ${nslookup_record} | egrep -c "(can't find|timed out)") == 0 ]]; then
-            echo "GOOD TO GO"
+        if [[ $(nslookup ${nslookup_record} | egrep -c "(can't find|timed out)") -eq 0 ]]; then
+            echo "GOOD TO GO";
             exit 0;
         fi
-        echo "Invalid response, will try again in 10 seconds. ($tries/30)"
-        sleep 10
+        echo "Invalid response, will try again in 10 seconds. ($tries/30)";
+        sleep 10;
     done
-    echo "ERROR !"
+    echo "ERROR !";
     exit 1;
 fi
