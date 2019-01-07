802b0a292a2b56823714eaac9ec0864dbb4cd187
Helder Sepulveda <heldersepu@gmail.com>
2019-01-06 19:44:45 -0500

    remove kms key and alias

--- TerraForm/kms.tf
+++ TerraForm/kms.tf
@@ -1,8 +1,10 @@
 resource "aws_kms_key" "data_bucket_kms" {
+  count       = 0 
   description = "This key is used to encrypt bucket data related bucket objects"
 }
 
 resource "aws_kms_alias" "data_bucket_kms" {
+  count         = 0
   name          = "alias/databucketkey"
   target_key_id = "${aws_kms_key.data_bucket_kms.key_id}"
 }
