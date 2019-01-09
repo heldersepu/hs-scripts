66fda259d96de217013c70ee1d0a924158b05724
Helder Sepulveda <heldersepu@gmail.com>
2019-01-08 20:45:00 -0500

    downgrade to 16.04
    Former-commit-id: 457cb88525e115fbd35e10b589a085b2c1c13e87

--- TerraForm/data.tf
+++ TerraForm/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-cosmic-18.10-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
 
   filter {
