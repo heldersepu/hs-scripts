59e75a8ab408dcad3b9a89741cc1304cae5cf9c5
Helder Sepulveda <heldersepu@gmail.com>
2019-01-11 12:43:03 -0500

    Testing aws_inspector_resource_group
    Former-commit-id: cdd49eed8d5039c47feca0f786acf8390fe6015c

---
+++ TerraForm/inspector.tf
@@ -0,0 +1,11 @@
+resource "aws_inspector_resource_group" "scheduled_inspector" {
+  tags {
+    Inspector   = "True"
+    Terraformed = "true"
+  }
+}
+
+resource "aws_inspector_assessment_target" "scheduled_inspector_target" {
+  name               = "Scheduled assessment target"
+  resource_group_arn = "${aws_inspector_resource_group.scheduled_inspector.arn}"
+}
