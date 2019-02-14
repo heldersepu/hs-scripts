6b4ba2bd381c203bf85f9aaa2625cd57b122c66d
Helder Sepulveda <heldersepu@gmail.com>
2019-02-14 03:59:23 -0500

    Testing underscores in the stage_name

--- TerraForm/api_gateway.tf
+++ TerraForm/api_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_stage" "test" {
-  stage_name    = "prod"
+  stage_name    = "prod_1"
   rest_api_id   = "${aws_api_gateway_rest_api.test.id}"
   deployment_id = "${aws_api_gateway_deployment.test.id}"
 }
