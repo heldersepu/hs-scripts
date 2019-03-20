4a98d119ff5fbef97049b9f7f218980018d8edcb
Helder Sepulveda <heldersepu@gmail.com>
2019-03-20 15:15:52 -0400

    Disable test_lambda

--- TerraForm/lambda_function.tf
+++ TerraForm/lambda_function.tf
@@ -23,6 +23,7 @@ locals {
 }
 
 resource "aws_lambda_function" "test_lambda" {
+  count            = 0
   filename         = "lambda_function_payload.zip"
   function_name    = "lambda_function_name"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
