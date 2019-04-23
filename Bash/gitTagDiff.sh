867eb135bc759799929b78d3e790c78545d21102
Helder Sepulveda <heldersepu@gmail.com>
2019-04-23 14:40:02 -0400

    Add sample and correct echo

--- Bash/gitTagDiff.sh
+++ Bash/gitTagDiff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# sh gitTagDiff.sh v0.5.0 v0.6.0 README
 
 IFS=$'\n'
 diff_arr=($(git diff $1 $2 --stat | grep $3))
@@ -8,10 +8,8 @@ IFS='|'
 for line in "${diff_arr[@]}"
 do
     data=($line)
-    file="$(echo -e "${data[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    file="$(echo "${data[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
     echo $file
     git diff -U0 $1 $2 -- "$file" | tail
     echo ""
 done
-
-
