7bd6a2ea9f2632685a2e5481e4586d5c23ba5ac2
Helder Sepulveda <heldersepu@gmail.com>
2019-05-21 15:06:55 -0400

    Testing resource with count & length
    Former-commit-id: 2a93f25849e83fc0c5d4fa4392a4a6b705dcb016

--- TerraForm/var_len/main.tf
+++ TerraForm/var_len/main.tf
@@ -4,6 +4,13 @@ variable "test" {
   type = "map"
 }
 
+resource "aws_key_pair" "test_sshkey" {
+  count      = "${length(var.test["de"])}"
+  key_name   = "sshkey_${count.index}"
+  public_key = "${file("~/Downloads/AWS_keys/test.pem.pub")}"
+}
+
+
 output "var_len" {
   value = "${length(var.test["de"])}"
 }
