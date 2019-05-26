c8868934ba5af6e7c7cd98a1ccc6bf6ad1fd3db6
Helder Sepulveda <heldersepu@gmail.com>
2019-05-25 22:25:29 -0400

    Initial logic for queue
    Stop all completed and those over the max 
    
    Former-commit-id: 6bae7cfb156e6077385492c73ad2241160bd0445

--- Bash/tribler_queue.sh
+++ Bash/tribler_queue.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-IFS=$'\n'
+MAX_QUEUE=1
 H=http://localhost:8085
+IFS=$'\n'
 
-for row in $(curl -s  $H/downloads | jq -c .downloads[]); do
+running=0
+downloads=$(curl -s $H/downloads | jq -c .downloads[])
+for row in $downloads; do
     echo ${row} | jq .name
-    id=$(echo ${row} | jq .id)
     status=$(echo ${row} | jq .status)
     progress=$(echo ${row} | jq .progress)
     echo $status  $progress
     echo ""
+
+    id=$(echo ${row} | jq .infohash | sed 's/^"\(.*\)"$/\1/')
+    if [ $progress -eq 1 ] ; then
+        # Stop all with progress completed
+        if [ $status != '"DLSTATUS_STOPPED"' ] ; then
+            resp=$(curl -sX PATCH $H/downloads/$id --data "state=stop")
+        fi
+    else
+        # Count the running and stop if we have more than max
+        if [ $status != '"DLSTATUS_STOPPED"' ] ; then
+            ((running++))
+            if [ $running -gt $MAX_QUEUE ] ; then
+                resp=$(curl -sX PATCH $H/downloads/$id --data "state=stop")
+            fi
+        fi
+    fi
 done
