bed81b8565a6b1c7990fea305a8ada4a38f35994
Helder Sepulveda <heldersepu@gmail.com>
2019-05-26 09:35:01 -0400

    Add final logic to the queue
    We keep an array of those stopped but not full downloaded then after the loop if we are under the max we start from that array until we reach the max

--- Bash/tribler_queue.sh
+++ Bash/tribler_queue.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 MAX_QUEUE=1
-H=http://localhost:8085
+H=http://localhost:8085/downloads
 IFS=$'\n'
 
 running=0
-downloads=$(curl -s $H/downloads | jq -c .downloads[])
+arr_queue=()
+downloads=$(curl -s $H | jq -c .downloads[])
 for row in $downloads; do
     echo ${row} | jq .name
     status=$(echo ${row} | jq .status)
@@ -14,18 +15,29 @@ for row in $downloads; do
     echo ""
 
     id=$(echo ${row} | jq .infohash | sed 's/^"\(.*\)"$/\1/')
-    if [ $progress -eq 1 ] ; then
+    if [ "$progress" == "1" ] ; then
         # Stop all with progress completed
         if [ $status != '"DLSTATUS_STOPPED"' ] ; then
-            resp=$(curl -sX PATCH $H/downloads/$id --data "state=stop")
+            resp=$(curl -sX PATCH $H/$id --data "state=stop")
         fi
     else
         # Count the running and stop if we have more than max
         if [ $status != '"DLSTATUS_STOPPED"' ] ; then
             ((running++))
             if [ $running -gt $MAX_QUEUE ] ; then
-                resp=$(curl -sX PATCH $H/downloads/$id --data "state=stop")
+                resp=$(curl -sX PATCH $H/$id --data "state=stop")
             fi
+        else
+            arr_queue+=($id)
         fi
     fi
 done
+
+# If we have running less than max we start  
+if [ $running -lt $MAX_QUEUE ] ; then
+    for queue in $arr_queue; do
+        resp=$(curl -sX PATCH $H/$queue --data "state=resume")
+        ((running++)) 
+        if [ $running -ge $MAX_QUEUE ] ; then break; fi
+    done
+fi
