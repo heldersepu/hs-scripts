eedd748492d2d51da6cf0c482b8371639705cb4a
Helder Sepulveda <heldersepu@gmail.com>
2019-08-21 10:38:49 -0400

    Testing content from local var

--- TerraForm/local_file.tf
+++ TerraForm/local_file.tf
@@ -1,4 +1,15 @@
+locals {
+  kubeconfig = <<KUBECONFIG
+apiVersion: v1
+clusters:
+- cluster:
+    server: foo
+    certificate-authority-data: bar
+  name: kubernetes
+KUBECONFIG
+}
+
 resource "local_file" "test" {
-    content     = "Hello World!"
+    content     = "${local.kubeconfig}"
     filename = "~/.test/hello.world"
 }
