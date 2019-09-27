89589f7528132ebc7b2d7049056e88ea1b5cd642
Helder Sepulveda <heldersepu@gmail.com>
2019-09-26 22:14:03 -0400

    Complete importing roleIds
    Former-commit-id: 7792586fb77ddf241d69d1799c9882b342c09e74

--- Bash/sumologic/users.sh
+++ Bash/sumologic/users.sh
@@ -1,5 +1,5 @@
-roles=$(curl -u "$sumo_key" -X GET https://api.us2.sumologic.com/api/v1/roles | jq -r ".data[] | @base64")
-users=$(curl -u "$sumo_key" -X GET https://api.us2.sumologic.com/api/v1/users | jq -r ".data[] | @base64")
+roles=$(curl -u "$sumo_key" -X GET https://api.us2.sumologic.com/api/v1/roles | jq '[.data[] | {name:.name, id:.id}]')
+users=$(curl -u "$sumo_key" -X GET 'https://api.us2.sumologic.com/api/v1/users?limit=200&sortBy=email' | jq -r ".data[] | @base64")
 
 for user in $users; do
     id=$(echo $user | base64 --decode | jq -r ".id")
@@ -13,16 +13,25 @@ for user in $users; do
     resource="${resource// /_}"
     resource="${resource/./_}"
     resource="${resource/@/_}"
+        
+    for role in $(echo $roleIds | jq -r ".[]"); do
+        name=$(echo $roles | jq ".[] | select(.id == \"$role\")" | jq '.name')
+        name=$(echo $name | tr '[:upper:]' '[:lower:]')
+        name="${name// /_}"
+        name="${name//./_}"
+        name="${name//\"/}"
+        name="\${sumologic_role.$name.id}"
+        roleIds="${roleIds//$role/$name}"
+    done
 
     echo "# ----  terraform import sumologic_user.$resource $id"
-
     echo "resource \"sumologic_user\" \"$resource\" {"
     echo "  first_name  = \"$fname\""
     echo "  last_name   = \"$lname\""
     echo "  email       = \"$email\""
     echo "  active      = $active"
     echo ""
-    echo "  role_ids   = []"
+    echo "  role_ids   = $roleIds"
     echo "}"
 
     echo ""
