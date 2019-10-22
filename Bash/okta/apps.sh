55f38903621026a86397b65488fd7f7d477cd98f
Helder Sepulveda <heldersepu@gmail.com>
2019-10-22 11:26:00 -0400

    Check app type
    Former-commit-id: 29db7e8a439a0e1919dd51024f3dbb48cb33d658

--- Bash/okta/apps.sh
+++ Bash/okta/apps.sh
@@ -6,23 +6,40 @@ for app in $apps; do
     if [ $status = "\"ACTIVE\"" ]; then
         id=$(echo $app | base64 --decode | jq ".id")
         name=$(echo $app | base64 --decode | jq ".name")
+        type=$(echo $app | base64 --decode | jq ".signOnMode")
         label=$(echo $app | base64 --decode | jq ".label")
         nameTpl=$(echo $app | base64 --decode | jq ".credentials.userNameTemplate.template")
         nameTplType=$(echo $app | base64 --decode | jq ".credentials.userNameTemplate.type")
         resource=$(clean "${name}_${label}")
+
+        echo "# ----  terraform import okta_app_saml.$resource $id"
+        ##  AUTO_LOGIN, BOOKMARK, BROWSER_PLUGIN, OPENID_CONNECT, RADIUS_FOR_PALO_ALTO, SAML_2_0
+        if [ $type = "\"SAML_2_0\"" ]; then
+            echo "resource \"okta_app_saml\" \"$resource\" {"
+        elif [ $type = "\"BROWSER_PLUGIN\"" ]; then
+            echo "resource \"okta_app_swa\" \"$resource\" {"
+        else
+            echo "resource \"okta_app_bookmark\" \"$resource\" {"
+        fi
+
+        if [ $type = "\"BOOKMARK\"" ]; then
+            url=$(echo $app | base64 --decode | jq ".settings.app.url")
+            echo "  url        = $url"
+        else
+            echo "  preconfigured_app        = $name"
+        fi
+        echo "  label                    = $label"
+
+        if [ $type = "\"SAML_2_0\"" ]; then
+            echo "  user_name_template       = $nameTpl"
+            echo "  user_name_template_type  = $nameTplType"
+        fi
 
         usersLink=$(echo $app | base64 --decode | jq "._links.users.href" | tr -d '"')
         users=$(curl -s -X GET $usersLink  -H "Authorization: SSWS $okta_token" | jq ".[].credentials.userName")
         groupsLink=$(echo $app | base64 --decode | jq "._links.groups.href" | tr -d '"')
         groups=$(curl -s -X GET $groupsLink  -H "Authorization: SSWS $okta_token" | jq ".[].id")
 
-        echo "# ----  terraform import okta_app_saml.$resource $id"
-        echo "resource \"okta_app_saml\" \"$resource\" {"
-        echo "  preconfigured_app        = $name"
-        echo "  label                    = $label"
-        echo "  user_name_template       = $nameTpl"
-        echo "  user_name_template_type  = $nameTplType"
-        
         for user in $users; do
             echo "  users {"
             echo "    id = data.okta_user.users[$user].id"
