347a02da4280f6bfcbb3e820af735784625b0120
Helder Sepulveda <heldersepu@gmail.com>
2019-11-04 21:52:02 -0500

    nested loop
    Former-commit-id: fe0cd8556f7325e50e59a2ab79ce02e123598707

--- TerraForm/for_each/main.tf
+++ TerraForm/for_each/main.tf
@@ -2,12 +2,19 @@ variable "list" {
   default = ["a", "b", "c"]
 }
 
+variable "list2" {
+  default = ["1", "2"]
+}
+
+
 resource "null_resource" "test" {
-  for_each = toset(var.list)
+  for_each = {
+    for x in setproduct(var.list, var.list2) : "${x[0]}.${x[1]}" => x
+  }
 
   provisioner "local-exec" {
     when        = "create"
-    command     = "echo ${each.key} ${each.value};"
+    command     = "echo ${each.key} = ${each.value[0]};"
     interpreter = ["/bin/sh", "-c"]
   }
 }
