900b9b77828e59f990bcb2802a3c7c13393daed4
Helder Sepulveda <heldersepu@gmail.com>
2019-11-06 11:36:59 -0500

    Complete policy codegen
    Former-commit-id: 28c773bea0b8f3588689616b578918a5dc7bfa86

--- Bash/okta/policies.sh
+++ Bash/okta/policies.sh
@@ -1,19 +1,41 @@
 source common.sh
 
-declare -a poltypes=("OKTA_SIGN_ON" "PASSWORD" "MFA_ENROLL" "IDP_DISCOVERY") # "OAUTH_AUTHORIZATION_POLICY" <- ERR
+declare -a poltypes=("OKTA_SIGN_ON" "MFA_ENROLL")
 for ptype in "${poltypes[@]}"; do
     echo $ptype
     policies=$(curl -s -X GET "$url/v1/policies?type=$ptype"  -H "Authorization: SSWS $okta_token" | jq -r ".[] | @base64")
     for pol in $policies; do
-        status=$(echo $zone | base64 --decode | jq ".status")
+        status=$(echo $pol | base64 --decode | jq ".status")
         if [ $status = "\"ACTIVE\"" ]; then
-            system=$(echo $zone | base64 --decode | jq ".system")
+            system=$(echo $pol | base64 --decode | jq ".system")
             if [ $system = "false" ]; then
                 id=$(echo $pol | base64 --decode | jq ".id")
-                name=$(echo $zone | base64 --decode | jq ".name")
+                name=$(echo $pol | base64 --decode | jq ".name")
+                desc=$(echo $pol | base64 --decode | jq ".description")
+                priority=$(echo $pol | base64 --decode | jq ".priority")
                 resource=$(clean "${name}")
 
-                echo "# ----  terraform import okta_.$resource $id"
+                if [ $ptype = "OKTA_SIGN_ON" ]; then
+                    echo "# ----  terraform import okta_policy_signon.$resource $id"
+                    echo "resource \"okta_policy_signon\" \"$resource\" {"
+                else
+                    echo "# ----  terraform import okta_policy_mfa.$resource $id"
+                    echo "resource \"okta_policy_mfa\" \"$resource\" {"
+                fi
+                echo "  name        = $name"
+                echo "  description = $desc"
+                echo "  priority    = $priority"
+
+                group=$(echo $pol | base64 --decode | jq ".conditions.people.groups.include[0]" | tr -d '"')
+                groupName=$(curl -s -X GET "$url/v1/groups/$group"  -H "Authorization: SSWS $okta_token" | jq ".profile.name")
+                echo "  groups_included = [ data.okta_group.groups[$groupName].id ]"
+
+                if [ $ptype = "MFA_ENROLL" ]; then
+                    echo "  ## manually add settings here ##"
+                fi
+
+                echo "}"
+                echo ""
             fi
         fi
     done
