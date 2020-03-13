73f847f1bd4700697065cf07f997d1f78d45ee6f
Helder Sepulveda <heldersepu@gmail.com>
2020-03-13 14:51:38 -0400

    reading accessKeys from sumologic

---
+++ Bash/sumologic/keys.sh
@@ -0,0 +1,12 @@
+sumo_key="id:key"
+base_url="https://api.us2.sumologic.com/api/v1/accessKeys"
+accessKeys=$(curl -u "$sumo_key" -X GET $base_url?limit=500 | jq -r ".data[] | @base64")
+
+for key in $accessKeys; do
+    id=$(echo $key | base64 --decode | jq -r ".id")
+    label=$(echo $key | base64 --decode | jq -r ".label")
+    echo $id  $label
+    if [ "$label" = "DELETE ME" ]; then
+        curl -u "$sumo_key" -X DELETE $base_url/$id
+    fi 
+done
