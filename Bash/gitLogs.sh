279b29f1d55c5658f0ed7b41bc3fc4e24a8a018d
Helder Sepulveda <heldersepu@gmail.com>
2020-03-20 18:14:07 -0400

    replace cut with head and set max at 10KB

--- Bash/gitLogs.sh
+++ Bash/gitLogs.sh
@@ -1,5 +1,5 @@
 add_git_metadata_message() {
-  local message=$(git log -1 --format=format:%B | cut -c 1-256 | jq -s -R .)
+  local message=$(git log -1 --format=format:%B | head -c 10240 | jq -s -R .)
 
   jq ". + [
     {name: \"message\", value: ${message}, type: \"message\"}
@@ -9,4 +9,3 @@ add_git_metadata_message() {
 
 jq -n "[]" | \
 add_git_metadata_message 
-    
