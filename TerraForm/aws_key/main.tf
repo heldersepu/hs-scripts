7f62eb30a473a871410dbfe7e670a18f4f030c28
Helder Sepulveda <heldersepu@gmail.com>
2020-05-06 11:36:12 -0400

    Testing random_string
    Former-commit-id: f86c50130314dd87850e4c6d15a161b8d28c79b2

---
+++ TerraForm/aws_key/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_string" "key" {
+  length  = 8
+  special = false
+  keepers = {
+    build_number = timestamp()
+  }
+}
+
+resource "null_resource" "download_key" {
+  triggers = {
+    build_number = timestamp()
+  }
+
+  provisioner "local-exec" {
+    when    = create
+    command = "aws s3api get-object --bucket bucket123456123654 --key dir/data ${path.module}/${random_string.key.result}"
+  }
+}
+
+resource "aws_key_pair" "project" {
+  count      = fileexists("${path.module}/${random_string.key.result}") ? 1 : 0
+  depends_on = [null_resource.download_key]
+  key_name   = "project"
+  public_key = file("${path.module}/${random_string.key.result}")
+
+  lifecycle {
+    ignore_changes = [public_key]
+  }
+}
