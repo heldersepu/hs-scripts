9f15009bbdf4e7e31dcd0da0050740a527ea0f64
Helder Sepulveda <heldersepu@gmail.com>
2020-05-16 18:26:29 -0400

    improve sieve
    Former-commit-id: 3a2c2d15f62a85b7033e70c6ccd00b2f3fb2233a

--- Python/codility/Eratosthenes.py
+++ Python/codility/Eratosthenes.py
@@ -1,12 +1,42 @@
+import timeit
+
+
 def solution(N):
-    primes = [2] + list(range(3, N, 2))
-    i = 1
+    primes = set(range(3, N, 2))
+    primes.add(2)
+    i = 3
     while i < len(primes):
-        for j in range(primes[i]*2, N, primes[i]):
+        for j in range(i**2, N, i):
             if j in primes:
                 primes.remove(j)
-        i += 1
+        i += 2
+    return primes
+
+
+def solutionB(N):
+    sieve = [False, True] * (N//2 + 1)
+    sieve[1] = False
+    sieve[2] = True
+    i = 3
+    while (i*i <= N):
+        if sieve[i] == True:
+            for j in range(i*i, N+1, i):
+                sieve[j] = False
+        i += 2
+    primes = []
+    for i in range(N):
+        if sieve[i]:
+            primes.append(i)
     return primes
 
 
 print(solution(100))
+print(solutionB(100))
+
+starttime = timeit.default_timer()
+solution(10000000)
+print(timeit.default_timer() - starttime)
+
+starttime = timeit.default_timer()
+solutionB(10000000)
+print(timeit.default_timer() - starttime)
