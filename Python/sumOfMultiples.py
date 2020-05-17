580205fb0f3130f6a30e775bba8913fcb103daba
Helder Sepulveda <heldersepu@gmail.com>
2020-05-17 16:34:32 -0400

    Testing sum of factors
    Former-commit-id: 3bfa4cb00efcf286dde22eb8cd2e53c03da6c592

---
+++ Python/sumOfMultiples.py
@@ -0,0 +1,20 @@
+import math
+
+
+def solutionL(N):
+    s = 0
+    for i in range(N+1):
+        if (i % 3 == 0) or (i % 5 == 0):
+            s += i
+    return s
+
+
+def solutionC(N):
+    def fsum(N, F):
+        n = (N // F) * F
+        return (n + F) * (n//F)//2
+    return fsum(N, 3) + fsum(N, 5) - fsum(N, 15)
+
+
+print(solutionL(151))
+print(solutionC(151))
