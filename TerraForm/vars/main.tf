d9643935c5079b79448c93949112594db670fd9a
Helder Sepulveda <heldersepu@gmail.com>
2020-06-05 15:30:59 -0400

    testing default on complex vars

--- TerraForm/vars/main.tf
+++ TerraForm/vars/main.tf
@@ -1,8 +1,31 @@
 variable "build-number" {
-  type    = "string"
+  type    = string
   default = "0"
 }
 
 output "test" {
-  value = "${var.build-number}"
+  value = var.build-number
+}
+
+
+variable complex {
+  type = object({
+    gid = string
+    access = object({
+      id   = string
+      type = string
+    })
+  })
+
+  default = {
+    gid = "00000003"
+    access = {
+      id   = "7ab1d382"
+      type = "Role"
+    }
+  }
+}
+
+output "complex" {
+  value = var.complex
 }
