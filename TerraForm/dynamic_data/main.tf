7c4b4eddbb8d6e766883fdc145e8c18e0818bd9c
Helder Sepulveda <heldersepu@gmail.com>
2020-06-08 09:52:06 -0400

    Double loop
    Former-commit-id: 694abd4f1ecc1bfbf015bfcae93d80898e48df8c

--- TerraForm/dynamic_data/main.tf
+++ TerraForm/dynamic_data/main.tf
@@ -1,12 +1,23 @@
 variable "server_ip_configs" {
   default = {
-    mgmt               = { ct = "1" }
-    applicationgateway = { ct = "1" }
-    monitor            = { ct = "1" }
-    app                = { ct = "3" }
+    mgmt               = { ct = 1 }
+    applicationgateway = { ct = 1 }
+    monitor            = { ct = 1 }
+    app                = { ct = 3 }
   }
 }
 
+locals {
+  server_ip_configs_mapped = flatten([
+    for server, count in var.server_ip_configs : [
+      for i in range(count.ct) : {
+        "name" = join("-", [server, i + 1])
+      }
+    ]
+  ])
+}
+
+
 output "myout" {
-  value = [for k, v in var.server_ip_configs : { "name" = join("-", [k, v.ct]) }]
+  value = local.server_ip_configs_mapped
 }
