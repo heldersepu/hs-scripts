f15813777929e42792858ed1c711ec04757906eb
Helder Sepulveda <heldersepu@gmail.com>
2020-06-14 14:41:42 -0400

    testing depends_on to self

--- TerraForm/multi_provider/test.tf
+++ TerraForm/multi_provider/test.tf
@@ -8,11 +8,23 @@ provider "aws" {
   alias  = "r2"
 }
 
-resource "aws_vpc" "test_vpc" {
-  provider   = "aws.r1"
-  cidr_block = "10.0.2.0/24"
 
-  tags {
+locals {
+  cidrs = [
+    "10.0.2.0/24",
+    "10.1.2.0/24",
+    "10.2.2.0/24",
+  ]
+}
+
+resource "aws_vpc" "test_vpc" {
+  count = length(local.cidrs)
+  //depends_on = [aws_vpc.test_vpc[count.index-1]]
+
+  provider   = aws.r1
+  cidr_block = local.cidrs[count.index]
+
+  tags = {
     Name = "test_vpc"
   }
 }
