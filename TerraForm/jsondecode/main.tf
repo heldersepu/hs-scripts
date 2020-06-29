b0b48368594afebff11c42e08ead746e9adbb259
Helder Sepulveda <hsepulveda@magicleap.com>
2020-06-29 09:19:11 -0400

    Testing jsondecode

---
+++ TerraForm/jsondecode/main.tf
@@ -0,0 +1,31 @@
+variable "prod" {
+  default = <<EOF
+{
+"subscription": "xxxxxx",
+"tshirt_size": "large",
+"env_prefix": "prod"
+}
+EOF
+}
+
+variable "global" {
+  default = <<EOF
+{
+  "tshirtsizes": {
+    "small": { "cpucount": 1 },
+    "medium": { "cpucount": 2 },
+    "large": { "cpucount": 3 }
+  }
+}
+EOF
+}
+
+locals {
+    tshirtsize = jsondecode(var.prod).tshirt_size
+    cpucount   = jsondecode(var.global).tshirtsizes[local.tshirtsize].cpucount
+}
+
+output "cpucount" {
+    value = local.cpucount
+}
+
