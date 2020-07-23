9071d47e99a7d236d8c6c699dd28b4d224507f74
Helder Sepulveda <heldersepu@gmail.com>
2020-07-23 11:56:36 -0400

    testing data "external"
    Former-commit-id: ece89165796fbc5a05bfaf12226d6dfa57afc94c

---
+++ TerraForm/encrypt_output/aws_user/encrypt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+eval "$(jq -r '@sh "ID=\(.id) SE=\(.se)"')"
+ 
+access=$(aws kms encrypt --key-id alias/xxxx --plaintext $ID --output text --query CiphertextBlob --region us-east-1)
+secret=$(aws kms encrypt --key-id alias/xxxx --plaintext $SE --output text --query CiphertextBlob --region us-east-1)
+
+jq -n --arg a "$access" --arg s "$secret" '{"access_value":$a,"secret_value":$s}'
