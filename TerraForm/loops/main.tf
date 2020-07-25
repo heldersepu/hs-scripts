61c7bd20d01ade16cc76d4e74b163e84e4c83ac7
Helder Sepulveda <heldersepu@gmail.com>
2020-07-24 22:24:32 -0400

    Testing dynamic loops
    Former-commit-id: ba8b06e5bc40f5322a7bac11b431235150c85d64

---
+++ TerraForm/loops/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region      = "us-east-1"
+}
+
+resource "aws_elastic_beanstalk_configuration_template" "mytemplate" {  
+  for_each = toset(["app1", "app2"])
+  name = "app-type1-base"
+  application = each.value
+  solution_stack_name = "64bit Amazon Linux 2015.09 v2.0.8"
+
+  dynamic "setting" {
+    for_each = toset(sort(["ELBSubnets", "Subnets", "VPCId"]))
+    content {
+      namespace = "aws:ec2:vpc"
+      name = setting.value
+      value = "blah"
+    }
+  }
+}
+
