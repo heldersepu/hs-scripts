599168d08b6337595a8f951f25ed3324a4119cb0
Helder Sepulveda <heldersepu@gmail.com>
2020-07-30 13:09:42 -0400

    testing aws_ec2_instance(name:
    Former-commit-id: 0537bd98498240574afac4f207264444539ba3b0

--- inspec/controls/ec2_type_tag.rb
+++ inspec/controls/ec2_type_tag.rb
@@ -2,8 +2,8 @@ control 'ec2_type_tag' do
   title 'Ensure ec2 instance type and tags.'
   impact 1.0
 
-  aws_ec2_instances.instance_ids.each do |instance_id|
-    describe aws_ec2_instance(instance_id) do
+  for i in 0..2
+    describe aws_ec2_instance(name: "server#{i}") do
       its('instance_type') { should eq 't2.micro' }
       its('tags') { should include(key: 'terraformed', value: 'n') }
       its('tags') { should include(key: 'test', value: /.*/) }
