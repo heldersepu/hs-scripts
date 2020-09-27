b70011bf68c14effd365c7400f3d48f0a6eba804
Helder Sepulveda <heldersepu@gmail.com>
2020-09-27 17:49:01 -0400

    Script to replace tags
    Former-commit-id: ad472fe50ddc11fe8418f4db8420140b3ee81039

---
+++ Bash/awsEc2RenameTags.sh
@@ -0,0 +1,20 @@
+## Example:
+#    Replace tag in all instances where names Match "Test" the "Blue" with "Red"
+#    . awsEc2RenameTags.sh Test Blue Red
+#  
+
+json=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*${1}*" "Name=instance-state-name,Values=running")
+
+arrids=($(echo $json | jq  -r ".Reservations[].Instances[].InstanceId"))
+for id in "${arrids[@]}"; do
+    echo "- $id"
+    instance=$(echo $json | jq -r ".Reservations[].Instances[] | select(.InstanceId == \"$id\" )")
+    arrtags=($(echo $instance | jq -c '.Tags[]'))
+    for tag in "${arrtags[@]}"; do
+        if [[ $tag == *"${2}"* ]]; then
+            echo "  - $tag"
+            aws ec2 delete-tags --resources $id --tags $tag  
+            aws ec2 create-tags --resources $id --tags ${tag//$2/$3}
+        fi
+    done
+done
