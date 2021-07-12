67e23a74cfe45527494c0aca57e8796c910dfe42
Helder Sepulveda <heldersepu@gmail.com>
2021-07-12 17:00:08 -0400

    Testing aws_secretsmanager_secret

---
+++ TerraForm/secrets/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+   region  = "us-east-1"
+}
+
+data "aws_region" "current" {}
+
+data "aws_secretsmanager_secret" "token" {
+  name = "/test/${data.aws_region.current.name}/token"
+
+  depends_on = [
+    aws_secretsmanager_secret.token,
+  ]
+}
+
+data "aws_secretsmanager_secret_version" "token" {
+  secret_id = data.aws_secretsmanager_secret.token.arn
+
+  depends_on = [
+    aws_secretsmanager_secret_version.token,
+  ]
+}
+
+resource "aws_secretsmanager_secret" "token" {
+  name        = "/test/${data.aws_region.current.name}/token"
+  description = "cache service auth token"
+}
+
+resource "aws_secretsmanager_secret_version" "token" {
+  secret_id     = aws_secretsmanager_secret.token.id
+  secret_string = "placeholder"
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
+
+output "test" {
+  value = data.aws_secretsmanager_secret_version.token.secret_string
+  sensitive = true
+}
