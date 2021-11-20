b9ec13a4111e8a2611445df64fe9ebf052c05674
Helder Sepulveda <heldersepu@gmail.com>
2021-11-20 09:16:53 -0500

    Testing aws_ami array

---
+++ TerraForm/aws_ami_array/main.tf
@@ -0,0 +1,36 @@
+provider "aws" { region = "us-east-2" }
+
+locals {
+    allowed_os = {
+        "amazon": {owner: "amazon",       filter: "amzn2-ami-hvm*"},
+        "suse":   {owner: "amazon",       filter: "*suse*"},
+        "RHEL":   {owner: "amazon",       filter: "*RHEL*"},
+        "ubuntu": {owner: "099720109477", filter: "*ubuntu-bionic-18.04-amd64-*"},
+    }
+}
+
+variable "ami_name" {
+  default = "ubuntu"
+
+  validation {
+    condition     = can(regex("amazon|suse|RHEL|ubuntu", var.ami_name))
+    error_message = "Invalid ami name, allowed_values = [amazon suse RHEL ubuntu]."
+  }
+}
+
+data "aws_ami" "os" {
+  for_each = local.allowed_os
+
+  most_recent = true
+  owners      = [each.value.owner]
+  filter {
+    name   = "name"
+    values = [each.value.filter]
+  }
+}
+
+resource "aws_instance" "linux" {
+  ami           = data.aws_ami.os[var.ami_name].id
+  instance_type = "t2.micro"
+  # ... todo add arguments here
+}
