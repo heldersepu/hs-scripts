b2affe10c1bfbf1b8d0902de056b9534d0dd0b62
Helder Sepulveda <heldersepu@gmail.com>
2021-12-01 13:55:07 -0500

    Testing conditional settings

--- TerraForm/loops/main.tf
+++ TerraForm/loops/main.tf
@@ -18,3 +18,19 @@ resource "aws_elastic_beanstalk_configuration_template" "mytemplate" {
   }
 }
 
+resource "aws_elastic_beanstalk_configuration_template" "mytemplate2" {
+  count = 3
+  name                = "app-type1-base"
+  application         = "each.value"
+  solution_stack_name = "64bit Amazon Linux 2015.09 v2.0.8"
+
+  dynamic "setting" {
+    for_each = count.index < 2 ? ["1"]: []
+    content {
+      namespace = "aws:ec2:vpc"
+      name      = setting.value
+      value     = "blah"
+    }
+  }
+}
+
