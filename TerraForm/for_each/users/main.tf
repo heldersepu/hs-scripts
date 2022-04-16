2b8ee0df29179cbbb5d76f811bda4987a608b2ca
Helder Sepulveda <heldersepu@gmail.com>
2022-04-16 17:11:58 -0400

    Testing aws_iam_user_login_profile

---
+++ TerraForm/for_each/users/main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_user" "users" {
+  for_each      = toset(["diogo", "max"])
+  name          = each.value
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "one_time_pwd" {
+  for_each = aws_iam_user.users
+
+  user                    = each.value.name
+  password_length         = 32
+  password_reset_required = true
+}
+
+output "passwords" {
+  value = aws_iam_user_login_profile.one_time_pwd
+}
