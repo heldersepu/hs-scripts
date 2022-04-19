7673137405c25d38a3f2d4800d7b99b8edf049a8
Helder Sepulveda <heldersepu@gmail.com>
2022-04-19 11:55:47 -0400

    Testing delete-virtual-mfa-device

--- TerraForm/users/main.tf
+++ TerraForm/users/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "users" {
-  for_each      = toset(["diogo", "max"])
+  for_each      = toset(["test1", "test2"])
   name          = each.value
   force_destroy = true
 }
@@ -12,6 +12,22 @@ resource "aws_iam_user_login_profile" "one_time_pwd" {
   password_reset_required = true
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "null_resource" "delete_virtual_mfa" {
+  for_each = aws_iam_user.users
+
+  provisioner "local-exec" {
+    when    = create
+    command = "aws iam delete-virtual-mfa-device --serial-number arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/${each.value.name} &>/dev/null"
+  }
+  depends_on = [
+    aws_iam_user_login_profile.one_time_pwd
+  ]
+}
+
+
 # output "passwords" {
 #   value = aws_iam_user_login_profile.one_time_pwd
 # }
+
