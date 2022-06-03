2740e64141bf3b0ae626aeb2810bd57a987b4431
Helder Sepulveda <heldersepu@gmail.com>
2022-06-03 15:50:57 -0400

    testing aws_instance with for_each

--- TerraForm/suse/ec2.tf
+++ TerraForm/suse/ec2.tf
@@ -3,34 +3,37 @@ provider "aws" {
 }
 
 data "aws_ami" "suse12" {
+  owners      = ["amazon"]
+  most_recent = true
   filter {
-    name   = "image-id"
-    values = ["ami-1c761163"]
+    name   = "name"
+    values = ["*suse*"]
   }
+}
+
+locals {
+  instances = toset(["one", "two"])
 }
 
 resource "aws_instance" "suse12" {
+  for_each          = local.instances
   ami               = data.aws_ami.suse12.id
   instance_type     = "m5d.large"
   availability_zone = "us-east-1a"
-
-  volume_tags {
-    Name = "suse12 volume tags"
-  }
+  tags              = { Name = each.value }
 }
 
 resource "aws_ebs_volume" "data-ndj" {
+  for_each          = local.instances
   type              = "gp2"
   size              = 110
   availability_zone = "us-east-1a"
 
-  tags {
-    Name = "suse12-data-ndj"
-  }
 }
 
 resource "aws_volume_attachment" "data-ndj" {
+  for_each    = local.instances
   device_name = "/dev/sdj"
-  instance_id = aws_instance.suse12.*.id[0]
-  volume_id   = aws_ebs_volume.data-ndj.*.id[0]
+  instance_id = aws_instance.suse12[each.key].id
+  volume_id   = aws_ebs_volume.data-ndj[each.key].id
 }
