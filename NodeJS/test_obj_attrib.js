6c2d752afaa38970b591e25155b876f1229399e8
Helder Sepulveda <heldersepu@gmail.com>
2022-08-18 04:59:07 -0400

    The value of the attribute affects output

--- NodeJS/test_obj_attrib.js
+++ NodeJS/test_obj_attrib.js
@@ -1,16 +1,19 @@
-const types = ["capped", "uncapped"]
+const types = ["capped", "uncapped", "test"]
 const contracts = {
     collection: {
         capped: "CappedCollection",
-        uncapped: "UncappedCollection"
+        uncapped: "UncappedCollection",
+        test: null
     }
 }
 
 function test(type) {
     console.log((types.includes(type))? "yes": "no")
     console.log((contracts.collection[type])? "yes": "no")
+    console.log("")
 }
 
 test("capped")
 test("uncapped")
 test("bad")
+test("test")
