fb7f69a81b661f708f63134cf16adc1d7d6d1c2f
Helder Sepulveda <heldersepu@gmail.com>
2022-09-20 11:27:01 -0400

    more complex setproduct

--- TerraForm/setproduct/main.tf
+++ TerraForm/setproduct/main.tf
@@ -1,15 +1,19 @@
 locals {
-  networks = ["avalanche", "fantom"]
+  networks = [
+    { "name" : "avalan", "factor" : 99 * 0.25 },
+    { "name" : "fantom", "factor" : 100 * 10 },
+  ]
   methods = [
-    "A_transferOwnership",
-    "A_deploySimpleMarketplace",
-    "B_addNFTContract"
+    { "name" : "A_transferOwnership", "gas" : 100 },
+    { "name" : "A_deploySimpleMarkt", "gas" : 80 },
+    { "name" : "B_addOneNFTContract", "gas" : 210 }
   ]
 
   all_keys = {
-    for x in setproduct(local.networks, local.methods) : "${x[1]}_${x[0]}" => {
-      network : x[0],
-      group : split("_", x[1])[0]
+    for x in setproduct(local.networks, local.methods) : "${x[1].name}_${x[0].name}" => {
+      network : x[0].name,
+      group : split("_", x[1].name)[0],
+      cost: x[0].factor * x[1].gas
     }
   }
 }
