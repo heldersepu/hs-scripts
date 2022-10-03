21b2a91954958a04b305c3442607b349021a4901
Helder Sepulveda <heldersepu@gmail.com>
2022-10-03 12:11:56 -0400

    Remove unused aws_lambda_function

--- TerraForm/lambda_function.tf
+++ TerraForm/lambda_function.tf
@@ -21,25 +21,3 @@ EOF
 locals {
   concurrent_exec = -1
 }
-
-resource "aws_lambda_function" "test_lambda" {
-  count            = 0
-  filename         = "lambda_function_payload.zip"
-  function_name    = "lambda_function_name"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "exports.test"
-  source_code_hash = base64sha256(file("lambda_function_payload.zip"))
-  runtime          = "nodejs8.10"
-
-  reserved_concurrent_executions = local.concurrent_exec
-
-  lifecycle {
-    ignore_changes = [reserved_concurrent_executions]
-  }
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
