a1a3a721511941ddadcf92af82298663d1845bad
Helder Sepulveda <heldersepu@gmail.com>
2022-12-17 12:36:28 -0500

    Create for_loop.tf

---
+++ TerraForm/nexted_for_loops/for_loop.tf
@@ -0,0 +1,45 @@
+locals {
+  vmDisks = {
+    for key, value in flatten([
+      for vmKey, vm in var.VirtualMachine : [
+        for diskKey, disk in vm.Disks.Data : {
+          VmId   = vmKey
+          DiskId = diskKey
+          disk  = disk
+          Lun    = 1 + index(keys(vm.Disks.Data), diskKey)
+        }
+      ]
+      if contains(keys(vm.Disks), "Data")
+    ]) : "${value.VmId}.${value.DiskId}" => value
+  }
+}
+
+output "test" {
+    value = local.vmDisks
+}
+
+variable "VirtualMachine" {
+  default = {
+    app = {
+      Size = "Standard_B4ms"
+      Disks = {
+        OS = {
+          Cache = "ReadWrite"
+          Size  = 128
+          Type  = "Premium_LRS"
+        }
+        Data = {
+          Cache = "ReadWrite"
+          Size  = 64
+          Type  = "Premium_LRS"
+        }
+      }
+      Image = {
+        Publisher = "MicrosoftWindowsServer"
+        Offer     = "WindowsServer"
+        Sku       = "2022-datacenter-azure-edition"
+        Version   = "latest"
+      }
+    }
+  }
+}
