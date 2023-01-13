2d35eff34d3d1ac034f430e2b29d02453543e072
Helder Sepulveda <heldersepu@gmail.com>
2023-01-13 10:49:18 -0500

    Testing aws_iam_role

---
+++ TerraForm/aws_iam_role/man.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda_123"
+
+  assume_role_policy = jsonencode({
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Action: "sts:AssumeRole",
+                Principal: {
+                    Service: "lambda.amazonaws.com"r
+                },
+                Effect: "Allow",
+                Sid: ""
+            }
+        ]
+    })
+}
