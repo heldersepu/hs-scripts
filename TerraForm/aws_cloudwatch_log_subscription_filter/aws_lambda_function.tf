70c1b6249b972adf67a462e68f5c1b26b5aceb81
Helder Sepulveda <heldersepu@gmail.com>
2023-04-21 23:54:43 -0400

    Correction to filename

--- TerraForm/aws_cloudwatch_log_subscription_filter/aws_lambda_function.tf
+++ TerraForm/aws_cloudwatch_log_subscription_filter/aws_lambda_function.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "lambda" {
   for_each      = toset(["reader", "writer"])
   function_name = each.value
 
-  filename = "lambda_function_payload.zip"
+  filename = "lambda.zip"
   role     = aws_iam_role.iam_for_lambda.arn
   handler  = "lambda.handler"
   runtime  = "python3.9"
