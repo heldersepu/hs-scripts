b515d4c6e0c57eac688bd73bf4aecdf8baacd1d1
Helder Sepulveda <heldersepu@gmail.com>
2023-04-22 09:26:32 -0400

    Add aws_lambda_permission

--- TerraForm/aws_cloudwatch_log_subscription_filter/aws_lambda_function.tf
+++ TerraForm/aws_cloudwatch_log_subscription_filter/aws_lambda_function.tf
@@ -15,3 +15,11 @@ resource "aws_lambda_function" "lambda" {
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 }
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  for_each      = toset(["reader", "writer"])
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value
+  principal     = "events.amazonaws.com"
+}
