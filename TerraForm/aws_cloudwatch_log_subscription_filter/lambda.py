70e2236eef17f36bfa9e24114351bff7560500f8
Helder Sepulveda <heldersepu@gmail.com>
2023-04-22 12:25:04 -0400

    Alternative to generate event

--- TerraForm/aws_cloudwatch_log_subscription_filter/lambda.py
+++ TerraForm/aws_cloudwatch_log_subscription_filter/lambda.py
@@ -4,7 +4,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    if (event == "ERROR"):
+    if ("ERROR" in event):
         logging.error("malfunction detected...")
     else:
         logging.info(event)
