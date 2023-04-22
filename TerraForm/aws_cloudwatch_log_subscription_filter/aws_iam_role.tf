9f53bbfba3e261314a300a4fa1fca9bd736c2798
Helder Sepulveda <heldersepu@gmail.com>
2023-04-22 13:26:00 -0400

    Add CloudWatchLogsFullAccess to the role

--- TerraForm/aws_cloudwatch_log_subscription_filter/aws_iam_role.tf
+++ TerraForm/aws_cloudwatch_log_subscription_filter/aws_iam_role.tf
@@ -12,3 +12,9 @@ resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+resource "aws_iam_role_policy_attachment" "CloudWatchLogsFullAccess" {
+  role       = aws_iam_role.assume_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
