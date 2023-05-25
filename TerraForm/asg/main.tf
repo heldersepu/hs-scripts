89ab54700487999532d68bd17e7a4d3883ccb7da
Helder Sepulveda <heldersepu@gmail.com>
2023-05-24 21:23:50 -0400

    testing aws_autoscaling_group

---
+++ TerraForm/asg/main.tf
@@ -0,0 +1,37 @@
+provider "aws" { region = "us-east-1" }
+
+data "aws_ami_ids" "ubuntu" {
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
+  }
+}
+
+resource "aws_launch_template" "foo" {
+  name          = "foo"
+  image_id      = data.aws_ami_ids.ubuntu.ids[0]
+  instance_type = "t2.micro"
+
+
+  metadata_options {
+    http_endpoint = "disabled"
+    http_tokens   = "required"
+  }
+
+  monitoring {
+    enabled = true
+  }
+}
+
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  launch_template {
+    id      = aws_launch_template.foo.id
+    version = "$Latest"
+  }
+}
