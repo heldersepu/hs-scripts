83e9b54b640cd7a45c815d4c6c1d37bc5daf0d21
Helder Sepulveda <heldersepu@gmail.com>
2023-07-12 13:36:09 -0400

    testing aws_lambda_layer_version

---
+++ TerraForm/lambda_layer/main.tf
@@ -0,0 +1,33 @@
+provider "aws" { region = "us-east-1" }
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda.py"
+  output_path = "lambda.zip"
+}
+
+data "archive_file" "layer" {
+  type        = "zip"
+  source_file = "layer.py"
+  output_path = "layer.zip"
+}
+
+resource "aws_lambda_layer_version" "layer" {
+  filename                 = data.archive_file.layer.name
+  layer_name               = "test_layer"
+  compatible_runtimes      = ["python3.9"]
+  compatible_architectures = ["x86_64"]
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "test"
+
+  filename = "lambda.zip"
+  role     = aws_iam_role.iam_for_lambda.arn
+  handler  = "lambda.handler"
+  runtime  = "python3.9"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  layers = [aws_lambda_layer_version.layer.arn]
+}
