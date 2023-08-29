0429a998bc97bae6dfb982372a0860c6c6ed9c18
Helder Sepulveda <heldersepu@gmail.com>
2023-08-29 08:44:10 -0400

    testing aws_iam_policy_document

---
+++ TerraForm/aws_iam_policy_document/main.tf
@@ -0,0 +1,27 @@
+provider "aws" { region = "us-east-1" }
+
+data "aws_iam_policy_document" "iam_policy_document" {
+  statement {
+    effect = "Deny"
+    actions = ["backup:*"]
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      values   = ["bucket-owner-full-control"]
+      variable = "s3:x-amz-acl"
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["xxxxxxxxxxxx"]
+      variable = "aws:SourceAccount"
+    }
+
+    condition {
+      test     = "ArnLike"
+      values   = ["arn:aws:s3:::my-tf-test-bucket"]
+      variable = "aws:SourceArn"
+    }
+  }
+}
