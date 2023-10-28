60bc5bd335be2436af8dfe6a0baeae09ac941397
Helder Sepulveda <heldersepu@gmail.com>
2023-10-28 13:30:29 -0400

    Sample terraform_data

---
+++ TerraForm/terraform_data/main.tf
@@ -0,0 +1,10 @@
+resource "terraform_data" "test" {
+  input = "test123"
+  provisioner "local-exec" {
+    command = "hostname"
+  }
+}
+
+output "test" {
+    value = terraform_data.test.output
+}
