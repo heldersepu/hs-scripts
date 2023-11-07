ecf2af9708a2367b887d8b7cf05d306f3c414567
Helder Sepulveda <heldersepu@gmail.com>
2023-11-07 10:59:31 -0500

    extracting parent folder

--- TerraForm/provider_javascript/main.tf
+++ TerraForm/provider_javascript/main.tf
@@ -4,12 +4,25 @@ terraform {
   }
 }
 
+locals {
+  files = fileset("${path.module}/..", "**")
+  parent_folders = toset([
+    for x in local.files: split("/", x)[0]
+    if strcontains(x, "/") && !startswith(x, ".")
+  ])
+}
+
 data "javascript" "MiXcAsE" {
-  for_each = fileset("${path.module}/..", "**")
+  for_each = local.files
   source = file("mixcase.js")
   vars = { input = each.key }
 }
 
 output "result" {
   value = [for x in data.javascript.MiXcAsE : x.result]
+}
+
+
+output "parent_folders" {
+  value = local.parent_folders
 }
