56aba3d5cdc5d9b67f160a510442e45b1087d213
Helder Sepulveda <heldersepu@gmail.com>
2023-12-30 18:24:51 -0500

    Testing multiple validations in one var

--- TerraForm/var_validation/main.tf
+++ TerraForm/var_validation/main.tf
@@ -9,6 +9,20 @@ variable "network_acls" {
     condition = alltrue([
       for v in var.network_acls : !(v.access == "Allow" && v.prefix == "*")
     ])
-    error_message = "Bad value ..."
+    error_message = "Bad value, Allow all is a bad practice ..."
+  }
+
+  validation {
+    condition = alltrue([
+      for v in var.network_acls : contains(["Allow", "Deny"], v.access)
+    ])
+    error_message = "Bad access, only [Allow,Deny]"
+  }
+
+  validation {
+    condition = alltrue([
+      for v in var.network_acls : contains(["Outbound", "Inbound"], v.direction)
+    ])
+    error_message = "Bad direction, only [Outbound,Inbound]"
   }
 }
