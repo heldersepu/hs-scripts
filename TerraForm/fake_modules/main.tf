95161b83d033790a4da432760ef243c766e23afe
Helder Sepulveda <heldersepu@gmail.com>
2024-02-28 11:49:00 -0500

    Testing fake modules

---
+++ TerraForm/fake_modules/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["/home/heldersepu/.aws/credentials"]
+}
+
+variable "vpcs" {
+  type = map(any)
+  default = {
+    a = {x = 1}
+    b = {x = 2}
+  }
+}
+
+variable "instances" {
+  type = map(any)
+  default = {
+    x = {vpc = "a"}
+    y = {vpc = "b"}
+    z = {vpc = "a"}
+  }
+}
+
+module "vpc" {
+  for_each = var.vpcs
+  source   = "git::https://github.com/gruntwork-io/terraform-fake-modules.git//modules/aws/vpc?ref=main"
+}
+
+module "eks" {
+  for_each = var.instances
+  source   = "git::https://github.com/gruntwork-io/terraform-fake-modules.git//modules/aws/eks?ref=main"
+  vpc_id   = module.vpc[each.value.vpc].id
+}
+
+output "test" {
+  value = module.eks
+}
