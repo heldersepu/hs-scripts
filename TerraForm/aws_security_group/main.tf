27087d518a806346e692b6c087783bd6be33f533
Helder Sepulveda <heldersepu@gmail.com>
2024-04-14 15:16:50 -0400

    format code

--- TerraForm/aws_security_group/main.tf
+++ TerraForm/aws_security_group/main.tf
@@ -13,19 +13,22 @@ locals {
   vpc_id = {
     tersu = data.aws_vpc.v
   }
-  all_rules = [[
-    for sg in var.security_groups : [
-      for i, ingress in sg.ingress :
-        merge({ "name" : sg.name, "type" : "ingress", "index": i }, ingress)
+  all_rules = [
+    [
+      for sg in var.security_groups : [
+        for i, ingress in sg.ingress :
+          merge({ "name" : sg.name, "type" : "ingress", "index" : i }, ingress)
+      ]
+    ],
+    [
+      for index, sg in var.security_groups : [
+        for i, egress in sg.egress :
+          merge({ "name" : sg.name, "type" : "egress", "index" : i }, egress)
+      ]
     ]
-    ], [
-    for index, sg in var.security_groups : [
-      for i, egress in sg.egress :
-        merge({ "name" : sg.name, "type" : "egress", "index": i }, egress)
-    ]
-  ]]
+  ]
   rules = {
-    for x in flatten(local.all_rules): "${x.name}_${x.type}_${x.index}" => x
+    for x in flatten(local.all_rules) : "${x.name}_${x.type}_${x.index}" => x
   }
 }
 
@@ -35,15 +38,15 @@ resource "aws_security_group" "sg" {
   vpc_id   = local.vpc_id[each.value.vpc].id
 }
 
- resource "aws_security_group_rule" "rule" {
+resource "aws_security_group_rule" "rule" {
   for_each          = local.rules
   security_group_id = aws_security_group.sg[each.value.name].id
 
-  type              = each.value.type
-  from_port         = each.value.from_port
-  to_port           = each.value.to_port
-  protocol          = each.value.protocol
-  cidr_blocks       = lookup(each.value, "cidr_blocks", null)
+  type        = each.value.type
+  from_port   = each.value.from_port
+  to_port     = each.value.to_port
+  protocol    = each.value.protocol
+  cidr_blocks = lookup(each.value, "cidr_blocks", null)
 
   source_security_group_id = lookup(each.value, "security_groups", null) == null ? null : (
     aws_security_group.sg[each.value.security_groups].id
