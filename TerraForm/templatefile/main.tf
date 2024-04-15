f736e34e7009faa3f1587ee95502763a0e3b6951
Helder Sepulveda <heldersepu@gmail.com>
2024-04-15 18:18:05 -0400

    testing conditional templatefile

--- TerraForm/templatefile/main.tf
+++ TerraForm/templatefile/main.tf
@@ -1,3 +1,8 @@
+variable "use_http_trigger" {
+  type    = bool
+  default = true
+}
+
 locals {
   tests = {
     aaa = { one = "111", two = "222" }
@@ -8,6 +13,12 @@ locals {
 resource "null_resource" "reference_test" {
   for_each = local.tests
   triggers = {
-    txt = templatefile("./foo.tftpl", { name = each.key, config = each.value })
+    txt = templatefile(
+      var.use_http_trigger ? "./foo.tftpl" : "./bar.tftpl",
+      {
+        name   = each.key,
+        config = each.value
+      }
+    )
   }
 }
