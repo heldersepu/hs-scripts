4b45933d0de99e67520b8843d46eb2018361eb83
Helder Sepulveda <heldersepu@gmail.com>
2024-06-17 21:18:52 -0400

    test create-access-point via cli

--- TerraForm/aws_s3_access_point/main.tf
+++ TerraForm/aws_s3_access_point/main.tf
@@ -8,7 +8,13 @@ resource "aws_s3_bucket" "example" {
   bucket = "example-${data.aws_caller_identity.current.account_id}"
 }
 
-resource "aws_s3_access_point" "example" {
-  bucket = aws_s3_bucket.example.id
-  name   = "example"
+# resource "aws_s3_access_point" "example" {
+#   bucket = aws_s3_bucket.example.id
+#   name   = "example"
+# }
+
+resource "null_resource" "name" {
+  provisioner "local-exec" {
+    command = "aws s3control create-access-point --account-id ${data.aws_caller_identity.current.account_id} --bucket \"${aws_s3_bucket.example.id}\" --name \"test-123\""
+  }
 }
