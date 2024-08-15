ce1a99b02968f1eecdaaff0e3e7b41866f27549f
Helder Sepulveda <heldersepu@gmail.com>
2024-08-15 18:15:58 -0400

    testing rds v

---
+++ Python/boto3/rds.py
@@ -0,0 +1,26 @@
+import boto3
+from moto import mock_aws
+
+@mock_aws
+def rds():
+    client = boto3.client("rds")
+    response = client.create_db_instance(
+        DBInstanceIdentifier="test",
+        DBInstanceClass="db.m5.large",
+        Engine="sqlserver-se",
+        MasterUsername='string',
+        MasterUserPassword='string123',
+        LicenseModel="license-included",
+        AllocatedStorage=32
+    )
+    print(response)
+    print("---")
+
+    response = client.modify_db_instance(
+        DBInstanceIdentifier="test",
+        CloudwatchLogsExportConfiguration={'EnableLogTypes':['error']}
+    )
+    print(response)
+    print("---")
+
+rds()
