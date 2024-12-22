d12de36be3ecba9b8fca735141174635e36a1a2a
Helder Sepulveda <heldersepu@gmail.com>
2024-12-21 20:17:42 -0500

    GCP List permissions

--- Bash/gcloud.sh
+++ Bash/gcloud.sh
@@ -1,1 +1,2 @@
 gcloud projects get-iam-policy "terraform-sandbox-441212" --flatten="bindings[].members" --format='table(bindings.role)' --filter="bindings.members:foo"
+gcloud iam roles describe roles/resourcemanager.projectIamAdmin
