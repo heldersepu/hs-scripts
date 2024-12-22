a5ecfb5821057281b8a0ea17c97c860decacd1ba
Helder Sepulveda <heldersepu@gmail.com>
2024-12-21 20:18:35 -0500

    GCP add the required role to the service account:

--- Bash/gcloud.sh
+++ Bash/gcloud.sh
@@ -1,2 +1,4 @@
 gcloud projects get-iam-policy "terraform-sandbox-441212" --flatten="bindings[].members" --format='table(bindings.role)' --filter="bindings.members:foo"
 gcloud iam roles describe roles/resourcemanager.projectIamAdmin
+
+# gcloud projects add-iam-policy-binding <YOUR GCLOUD PROJECT ID> --member=serviceAccount:foo  --role=roles/resourcemanager.projectIamAdmin
