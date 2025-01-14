304aaac4a25181d90150aa4b742cf38c40c9f87b
Helder Sepulveda <heldersepu@gmail.com>
2025-01-14 13:29:29 -0500

    add missing secondary_ip_range

--- TerraForm/gcp_helm/main.tf
+++ TerraForm/gcp_helm/main.tf
@@ -21,6 +21,16 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.0.0.0/20"
+
+  secondary_ip_range {
+    range_name    = "pod-range"
+    ip_cidr_range = "10.1.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "service-range"
+    ip_cidr_range = "10.2.0.0/20"
+  }
 }
 
 resource "google_service_account" "gke_sa_ba" {
