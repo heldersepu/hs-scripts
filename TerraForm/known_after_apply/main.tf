d2c15560ed98f9082347708854a00eeafe013191
Sepulveda, Helder <Helder.Sepulveda@transunion.com>
2026-03-10 18:12:49 -0400

    more complex output

--- TerraForm/known_after_apply/main.tf
+++ TerraForm/known_after_apply/main.tf
@@ -1,8 +1,21 @@
+variable "input" {
+    default = {}
+}
+
 resource "random_string" "key" {
   length  = 32
   special = false
 }
 
+locals {
+  platform = {
+    x = {
+      abc = try(var.input.run, false) ? random_string.key.result : "",
+      def = random_string.key.result
+    }
+  }
+}
+
 output "test" {
-  value = random_string.key.result
+  value = local.platform
 }
