49a8f88063b20409279806d5bda94590cc74fb7d
Sepulveda, Helder <Helder.Sepulveda@transunion.com>
2026-09-12 11:09:31 -0400

    Testing different sha256

--- TerraForm/template_file/main.tf
+++ TerraForm/template_file/main.tf
@@ -16,7 +16,30 @@ resource "local_file" "example" {
   content  = local.template_file
 }
 
+resource "null_resource" "setup_wiz_outpost_lite" {
+  depends_on = [local_file.example]
+  triggers = {
+    runners = sha256(local.template_file)
+  }
+  provisioner "local-exec" {
+    when    = create
+    command = "echo create"
+  }
+}
+
 output "file" {
   value     = local.template_file
   sensitive = true
+}
+
+output "content_sha256" {
+  value = local_file.example.content_sha256
+}
+
+output "local_sha256" {
+  value = nonsensitive(sha256(local.template_file))
+}
+
+output "filesha256" {
+  value = filesha256("./test.sh")
 }
